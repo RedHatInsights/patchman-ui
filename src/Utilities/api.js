920efc15a9135c21b2bdbe207217363f1f7840b8
Jiri Dostal <jdostal@redhat.com>
2019-12-06 15:30:08 +0100

    Use Vulnerability API temporarily

--- src/Utilities/api.js
+++ src/Utilities/api.js
@@ -1,11 +1,63 @@
-export const fetchApplicableSystems = () => {
-    return fetch('/api/patch/v1/advisories', {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Methods': 'POST, GET'
+export function createApiCall(
+    endpoint,
+    method,
+    parameters = undefined,
+    data = undefined
+) {
+    if (parameters && method === 'get') {
+        let params = Object.keys(parameters)
+        .map(key => [
+            encodeURIComponent(key)
+            .concat('=')
+            .concat(encodeURIComponent(parameters[key]))
+        ])
+        .join('&');
+        endpoint = endpoint.concat('?').concat(params);
+    }
+
+    let result = window.insights.chrome.auth
+    .getUser()
+    .then(() =>
+        fetch('/api/vulnerability/v1' + endpoint, {
+            method,
+            credentials: 'include',
+            body: JSON.stringify(data)
+        })
+    )
+    .then(res => {
+        if (!res.ok) {
+            const contentType = res.headers.get('content-type');
+            if (contentType.indexOf('json') !== -1) {
+                throw res.json();
+            } else {
+                throw new Promise(resolve =>
+                    resolve({
+                        errors: [
+                            { status: res.status, detail: res.statusText }
+                        ]
+                    })
+                );
+            }
         }
+
+        return res.json();
     })
-    .then(res => res.json())
-    .then(res => res);
+    .catch(error => {
+        error = Promise.resolve(error || {});
+        const genericError = {
+            detail:
+                    'There was an error getting data. Reload the page and try again'
+        };
+        return error.then(error => {
+            const res = (error.errors && error.errors[0]) || genericError;
+            throw { ...res };
+        });
+    });
+    return result;
+}
+
+export const fetchApplicableAdvisoriesApi = params => {
+    // eslint-disable-next-line no-console
+    console.log(params);
+    return createApiCall('/vulnerabilities/cves', 'get', params);
 };
