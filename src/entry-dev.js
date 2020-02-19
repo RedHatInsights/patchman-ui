145d302b6bb175460c53e4f9dc80bfb205b6a529
Jiri Dostal <jdostal@redhat.com>
2020-02-19 11:38:58 +0100

    Remove redux-logger and update dependencies

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { init } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import logger from 'redux-logger';
+import { init } from './store';
 import getBaseName from './Utilities/getBaseName';
 
 ReactDOM.render(
-    <Provider store={ init(logger).getStore() }>
-        <Router basename={ getBaseName(window.location.pathname) }>
-            <App/>
+    <Provider store={init().getStore()}>
+        <Router basename={getBaseName(window.location.pathname)}>
+            <App />
         </Router>
     </Provider>,
 
