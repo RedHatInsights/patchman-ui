b90a70b95a78bbe0f18b9a65768ddb7e2bebaea5
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-04 13:19:56 +0200

    Allow multiple vendors chunks.

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,5 +1,6 @@
 /* global require, module, __dirname */
 const { resolve } = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
@@ -8,7 +9,33 @@ const { config: webpackConfig, plugins } = config({
 const customPlugins = plugins.slice();
 const HtmlWebpackPlugin = customPlugins[5]; //htmlwebpackplugin workaround
 HtmlWebpackPlugin.options.chunksSortMode = 'none';
-module.exports = {
-    ...webpackConfig,
-    plugins: customPlugins
+module.exports = function(env) {
+    const plugins = [...customPlugins];
+    if (env && env.analyze === 'true') {
+        plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    /**
+     * stop forcing all node_modules to be part of a single chunk
+     */
+    webpackConfig.optimization.minimize = true;
+    webpackConfig.optimization.splitChunks = {
+        minSize: 30000,
+        cacheGroups: {
+            defaultVendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: 10,
+                reuseExistingChunk: true
+            },
+            default: {
+                priority: 20,
+                reuseExistingChunk: true
+            }
+        }
+    };
+
+    return {
+        ...webpackConfig,
+        plugins
+    };
 };
