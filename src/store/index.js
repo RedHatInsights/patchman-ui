babdc07a280358eedc15ee9d052e09a21d51a368
Jiri Dostal <jdostal@redhat.com>
2020-06-09 10:54:50 +0200

    feat(filtering): introduce persistent filtering

--- src/store/index.js
+++ src/store/index.js
@@ -10,21 +10,28 @@ import { SystemDetailStore } from './Reducers/SystemDetailStore';
 import { SystemsListStore } from './Reducers/SystemsListStore';
 
 let registry;
+const persistenceMiddleware = store => next => action => {
+    next(action);
+    const storeContent = store.getState();
+    sessionStorage.setItem('PatchStore', JSON.stringify(storeContent));
+};
 
 export function init(...middleware) {
     if (registry) {
         throw new Error('store already initialized');
     }
 
-    registry = new ReducerRegistry({}, [promiseMiddleware(), notificationsMiddleware(), ...middleware]);
+    registry = new ReducerRegistry({}, [promiseMiddleware(), notificationsMiddleware(), persistenceMiddleware, ...middleware]);
+
+    const storage = JSON.parse(sessionStorage.getItem('PatchStore')) || {};
 
     registry.register({
-        AdvisoryListStore,
-        SystemsListStore,
-        SystemDetailStore,
-        SystemAdvisoryListStore,
-        AdvisoryDetailStore,
-        AffectedSystemsStore,
+        AdvisoryListStore: (state = storage.AdvisoryListStore, action) => AdvisoryListStore,
+        SystemsListStore: (state = storage.SystemsListStore, action) => SystemsListStore(state, action),
+        SystemDetailStore: (state = storage.SystemDetailStore, action) => SystemDetailStore(state, action),
+        SystemAdvisoryListStore: (state = storage.SystemAdvisoryListStore, action) => SystemAdvisoryListStore(state, action),
+        AdvisoryDetailStore: (state = storage.AdvisoryDetailStore, action) => AdvisoryDetailStore(state, action),
+        AffectedSystemsStore: (state = storage.AffectedSystemsStore, action) => AffectedSystemsStore(state, action),
         notifications
     });
 
