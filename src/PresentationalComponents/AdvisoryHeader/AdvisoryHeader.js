17d9571076ea8e2d202e2dc92822da2f271f1063
Jiri Dostal <jdostal@redhat.com>
2020-07-23 11:28:00 +0200

    feat(advisoryDetail): Add information about CVEs

--- src/PresentationalComponents/AdvisoryHeader/AdvisoryHeader.js
+++ src/PresentationalComponents/AdvisoryHeader/AdvisoryHeader.js
@@ -1,3 +1,6 @@
+import { List, ListItem } from '@patternfly/react-core/dist/js/components/List';
+import { Modal, ModalVariant } from '@patternfly/react-core/dist/js/components/Modal';
+import { Text, TextContent, TextVariants } from '@patternfly/react-core/dist/js/components/Text/';
 import { Grid } from '@patternfly/react-core/dist/js/layouts/Grid/Grid';
 import { GridItem } from '@patternfly/react-core/dist/js/layouts/Grid/GridItem';
 import { Stack } from '@patternfly/react-core/dist/js/layouts/Stack/Stack';
@@ -13,7 +16,10 @@ import InfoBox from '../InfoBox/InfoBox';
 import AdvisorySeverityInfo from '../Snippets/AdvisorySeverityInfo';
 
 const AdvisoryHeader = ({ attributes, isLoading }) => {
+    const [isModalOpen, setModalOpen] = React.useState(false);
     const severityObject = getSeverityById(attributes.severity);
+    const cveCount = attributes.cves && attributes.cves.length;
+    const cvesLink = `${cveCount} CVE${cveCount !== 1 && 's' || ''} associated with this advisory`;
     return (
         <Grid hasGutter style={{ minHeight: 150 }}>
             <GridItem md={8} sm={12}>
@@ -51,7 +57,31 @@ const AdvisoryHeader = ({ attributes, isLoading }) => {
                         <StackItem>
                             <PortalAdvisoryLink advisory={attributes.id} />
                         </StackItem>
+                        <StackItem>
+                            <TextContent>
+                                <Text component={TextVariants.h2}>CVEs</Text>
+                                {cveCount === 0 ? cvesLink :
+                                    <a onClick={()=>setModalOpen(!isModalOpen)}>
+                                        {cvesLink}
+                                    </a>}
+                            </TextContent>
+
+                        </StackItem>
                     </Stack>
+                    <Modal
+                        variant={ModalVariant.small}
+                        title="CVEs"
+                        isOpen={isModalOpen}
+                        onClose={()=>setModalOpen(false)}
+                    ><List>
+                            {attributes.cves && attributes.cves.map(cve =>
+                                <ListItem key={cve}><a href={`${document.baseURI}insights/vulnerability/cves/${cve}`}>
+                                    {cve}
+                                </a>
+                                </ListItem>
+                            )}
+                        </List>
+                    </Modal>
                 </WithLoader>
             </GridItem>
             <GridItem md={4} sm={12}>
