8ad77ffd9b66ca4b5eedd355fb7176806440701a
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-08-24 21:39:40 +0200

    fix(ChromeNav) navigation is fixed on page change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import './App.scss';
-import { Routes } from './Routes';
+import { Routes, paths } from './Routes';
 
 // console.log('dev mode');
 class App extends Component {
@@ -13,20 +13,38 @@ class App extends Component {
         insights.chrome.identifyApp('patch');
         insights.chrome.navigation(buildNavigation());
 
-        this.appNav = insights.chrome.on('APP_NAVIGATION', event => {
-            if (event.domEvent) {
-                this.props.history.push(`/${event.navId}`);
-            }
-        }
-        );
-        this.buildNav = this.props.history.listen(() =>
-            insights.chrome.navigation(buildNavigation())
-        );
+        this.triggerNavigation();
+        this.unregister = this.listenNavigation();
     }
 
     componentWillUnmount() {
-        this.appNav();
-        this.buildNav();
+        this.unregister();
+    }
+
+    listenNavigation() {
+        return  insights.chrome.on('APP_NAVIGATION', event => {
+            if (event.domEvent) {
+                this.props.history.push(`/${event.navId}`);
+            }
+        });
+    }
+
+    triggerNavigation() {
+        this.props.history.listen(() => {
+
+            insights.chrome.navigation(buildNavigation());
+
+            const { pathname } = this.props.location;
+            const currentRoute = Object.values(paths).filter(element => pathname !== '/' && pathname.includes(element.to));
+
+            if (pathname === '/') {
+                insights.chrome.appNavClick({ id: 'advisories' }, false);
+            }
+            else if (currentRoute) {
+                const navId =  pathname.split('/').filter(element => element.length > 0)[0];
+                navId && insights.chrome.appNavClick({ id: navId });
+            }
+        });
     }
 
     render() {
@@ -37,10 +55,12 @@ class App extends Component {
             </React.Fragment>
         );
     }
+
 }
 
 App.propTypes = {
-    history: PropTypes.object
+    history: PropTypes.object,
+    location: PropTypes.object
 };
 
 export default withRouter(connect()(App));
