cb86d2cb3ad20453d49e0c41f659e6c456bb2d37
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-08-31 22:34:40 +0200

    fix(LastSeen): sorting bug fix by displaying last_upload instead of updated

--- src/store/Reducers/InventoryEntitiesReducer.js
+++ src/store/Reducers/InventoryEntitiesReducer.js
@@ -8,7 +8,17 @@ export const init = {
 };
 // Reducer
 
-function modifyInventory(columns, state) {
+function replaceLastUpdated (InventoryHosts, PatchHosts) {
+    return InventoryHosts.map((InventoryRow) => {
+        const patchLastSeen = PatchHosts && PatchHosts.find(patchRow=> patchRow.id === InventoryRow.id);
+        return {
+            ...InventoryRow,
+            updated: patchLastSeen && patchLastSeen.attributes.last_upload || InventoryRow.updated
+        };
+    });
+}
+
+function modifyInventory(columns, hosts, state) {
     if (state.loaded) {
         let lastSeenColumn = state.columns.filter(({ key }) => key === 'updated');
         lastSeenColumn = [{ ...lastSeenColumn[0], transforms: [sortable] }];
@@ -17,17 +27,23 @@ function modifyInventory(columns, state) {
             columns: [
                 ...columns || [],
                 ...lastSeenColumn || []
-            ]
+            ],
+            rows: replaceLastUpdated(state.rows, hosts)
         };
     }
 
     return state;
 }
 
-export const inventoryEntitiesReducer = columns => (state = init, action) => {
+export const inventoryEntitiesReducer = (columns, currentPage) => (state = init, action) => {
+    const store = action.store && action.store.getState();
+
+    //display patch 'last_upload' instead of Inventory 'updated' column
+    const PatchStore  = store && (currentPage === 'SYSTEMS_PAGE' ? store.SystemsListStore : store.AffectedSystemsStore);
+
     switch (action.type) {
         case 'LOAD_ENTITIES_FULFILLED':
-            return modifyInventory(columns, state);
+            return modifyInventory(columns, PatchStore.rows, state);
 
         default:
             return state;
