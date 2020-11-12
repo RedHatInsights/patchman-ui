cf7ca1fb289adf296b628bf9044d6fd5b7a01070
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-11-12 09:56:43 +0100

    fix(ConditionalFilter): radio typw filter empty value is fixed

--- src/PresentationalComponents/Filters/PublishDateFilter.js
+++ src/PresentationalComponents/Filters/PublishDateFilter.js
@@ -4,13 +4,8 @@ import { publicDateOptions } from '../../Utilities/constants';
 const publishDateFilter = (apply, currentFilter = {}) => {
     let { public_date: currentValue } = currentFilter;
 
-    // Empty string value is not supported by PF Radio at the moment
-    if (currentValue === '' || !currentValue) {
-        currentValue = '0';
-    }
-
     const filterByPublicDate = value => {
-        apply({ filter: { public_date: (value !== '0' && value) || '' } });
+        apply({ filter: { public_date: (value !== 'all' && value) || '' } });
     };
 
     return {
