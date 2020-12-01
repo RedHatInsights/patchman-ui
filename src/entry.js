044aadbd0ae2e08f5e3691c951910e2c73db2ea2
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-12-01 10:49:06 +0100

    feat(internationalization): install dependencies and adopt intl

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-handler-names */
+/* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -5,13 +7,16 @@ import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
 import getBaseName from './Utilities/getBaseName';
+import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
+import messages from '../locales/en.json';
 
 ReactDOM.render(
-    <Provider store={ init().getStore() }>
-        <Router basename={ getBaseName(window.location.pathname) }>
-            <App />
-        </Router>
-    </Provider>,
-
+    <IntlProvider locale = {navigator.language.slice(0, 2)}  messages={messages} onError={console.log} >
+        <Provider store={ init().getStore() }>
+            <Router basename={ getBaseName(window.location.pathname) }>
+                <App />
+            </Router>
+        </Provider>
+    </IntlProvider>,
     document.getElementById('root')
 );
