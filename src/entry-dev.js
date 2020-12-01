8cb32e1523ddc5cb5456846dba23ec0e04ca8832
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-12-01 10:49:06 +0100

    feat(internationalization): install dependencies and adopt intl

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -5,13 +5,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { init } from './store';
 import getBaseName from './Utilities/getBaseName';
+import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
+import messages from '../locales/en.json';
 
 ReactDOM.render(
-    <Provider store={init().getStore()}>
-        <Router basename={getBaseName(window.location.pathname)}>
-            <App />
-        </Router>
-    </Provider>,
+    <IntlProvider locale = {navigator.language.slice(0, 2)}  messages={messages}>
+        <Provider store={init().getStore()}>
+            <Router basename={getBaseName(window.location.pathname)}>
+                <App />
+            </Router>
+        </Provider>
+    </IntlProvider>,
 
     document.getElementById('root')
 );
