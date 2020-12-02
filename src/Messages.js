163a154efbb92d1d2dd3201249f4bcfa285e12cc
Muslimjon Kholjuraev <mkholjur@redhat.com>
2020-12-02 10:09:53 +0100

    feat(internationalization): autogenerate translations

---
+++ src/Messages.js
@@ -0,0 +1,50 @@
+/* eslint-disable max-len */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+    yes: {
+        id: 'yes',
+        description: 'Yes general label',
+        defaultMessage: 'Yes'
+    },
+    no: {
+        id: 'no',
+        description: 'No general label',
+        defaultMessage: 'No'
+    },
+    cancel: {
+        id: 'cancel',
+        description: 'Cancel label for general usage',
+        defaultMessage: 'Cancel'
+    },
+    save: {
+        id: 'save',
+        description: 'Save label for general usage',
+        defaultMessage: 'Save'
+    },
+    edit: {
+        id: 'edit',
+        description: 'Edit label for general usage',
+        defaultMessage: 'Edit'
+    },
+    delete: {
+        id: 'delete',
+        description: 'Delete label for general usage',
+        defaultMessage: 'Delete'
+    },
+    search: {
+        id: 'search',
+        description: 'Search label for general usage',
+        defaultMessage: 'Search'
+    },
+    loading: {
+        id: 'loading',
+        description: 'Loading label for general usage',
+        defaultMessage: 'Loading...'
+    },
+    count: {
+        id: 'count',
+        description: 'Label for the number of systems',
+        defaultMessage: 'Num. systems'
+    }
+});
