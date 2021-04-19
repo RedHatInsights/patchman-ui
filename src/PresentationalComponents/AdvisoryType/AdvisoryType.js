75b5a1b1eb06c6bad0ee145b020300102cdaac5d
mkholjur <mkholjur@redhat.com>
2021-04-19 23:41:35 +0200

    fix(Advisories): SPM-837 make type column one line

--- src/PresentationalComponents/AdvisoryType/AdvisoryType.js
+++ src/PresentationalComponents/AdvisoryType/AdvisoryType.js
@@ -1,18 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { advisoryTypes } from '../../Utilities/constants';
-import { Flex, FlexItem } from '@patternfly/react-core';
+import { Split, SplitItem } from '@patternfly/react-core';
 
 const AdvisoryType = ({ type }) => {
     const advisoryType =
         advisoryTypes.find(item => item.value === type) || advisoryTypes[2];
     return (
-        <React.Fragment>
-            <Flex>
-                <FlexItem spacer={{ default: 'spacerSm' }}>{advisoryType.icon}</FlexItem>
-                <FlexItem spacer={{ default: 'spacerSm' }}>{advisoryType.label}</FlexItem>
-            </Flex>
-        </React.Fragment>
+        <Split hasGutter className='advisoryTypeSpacer'>
+            <SplitItem>{advisoryType.icon}</SplitItem>
+            <SplitItem isFilled>{advisoryType.label}</SplitItem>
+        </Split>
     );
 };
 
