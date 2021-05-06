3a00a6d4d2454cdf3a7391d455dda49b01efa011
Karel Hala <khala@redhat.com>
2021-05-06 13:08:39 +0200

    feat(system detail): use federated modules to share system detail

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import SystemDetail from './SmartComponents/SystemDetail/SystemDetail';
-const WrappedSystemDetail = (props) => {
+import { SystemAdvisoryListStore } from './store/Reducers/SystemAdvisoryListStore';
+import { SystemPackageListStore } from './store/Reducers/SystemPackageListStore';
+import { Bullseye, Spinner } from '@patternfly/react-core';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const WrappedSystemDetail = ({ getRegistry, ...props }) => {
+    const [Wrapper, setWrapper] = useState();
+    useEffect(() => {
+        if (getRegistry) {
+            getRegistry()?.register?.({ SystemAdvisoryListStore, SystemPackageListStore });
+        }
+
+        setWrapper(() => getRegistry ? Provider : Fragment);
+    }, []);
     return <Router>
-        <SystemDetail {...props} isInventoryApp/>
+        {
+            Wrapper ? <Wrapper {...getRegistry && { store: getRegistry()?.getStore() }}>
+                <SystemDetail {...props} isInventoryApp />
+            </Wrapper> : <Bullseye>
+                <Spinner size="xl" />
+            </Bullseye>
+        }
     </Router>;
 };
 
-export { SystemAdvisoryListStore } from './store/Reducers/SystemAdvisoryListStore';
-export { SystemPackageListStore } from './store/Reducers/SystemPackageListStore';
+WrappedSystemDetail.propTypes = {
+    getRegistry: PropTypes.func
+};
+
+export { SystemPackageListStore, SystemAdvisoryListStore };
 
 export default WrappedSystemDetail;
