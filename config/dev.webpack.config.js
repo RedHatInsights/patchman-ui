428b052759e90857d7c900b07f66d7b99462d13e
Jiri Dostal <jdostal@redhat.com>
2021-07-30 11:41:01 +0200

    feat(webpack): SPM-1062 Switch to webpack proxy and enable auto reload

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,11 +1,31 @@
 /* global require, module, __dirname */
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+
+const insightsProxy = {
+    https: false,
+    port: process.env.FRONTEND_PORT ? process.env.FRONTEND_PORT : '8002',
+    ...(process.env.BETA && { deployment: 'beta/apps' })
+};
+
+const webpackProxy = {
+    deployment: process.env.BETA ? 'beta/apps' : 'apps',
+    appUrl: process.env.BETA ? ['/beta/insights/patch'] : ['/insights/patch'],
+    env: `ci-${process.env.BETA ? 'beta' : 'stable'}`,
+    useProxy: true,
+    proxyVerbose: true,
+    useCloud: true, // until console pre-prod env is ready
+    // localChrome: '~/insights/insights-chrome/build/', // for local chrome builds
+    routes: {
+        //   '/beta/config': { host: 'http://localhost:8003' }, // for local CSC config
+    }
+};
+
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
     debug: true,
-    https: true,
-    ...(process.env.BETA && { deployment: 'beta/apps' })
+    useFileHash: false,
+    ...(process.env.PROXY ? webpackProxy : insightsProxy)
 });
 
 plugins.push(
