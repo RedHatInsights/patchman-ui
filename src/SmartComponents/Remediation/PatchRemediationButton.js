cfe883771ed95cd5a5dfdbfc444a162744de1620
Jiri Dostal <jdostal@redhat.com>
2021-08-10 10:56:37 +0200

    feat(remediation): SPM-1050 redesign remediation button

--- src/SmartComponents/Remediation/PatchRemediationButton.js
+++ src/SmartComponents/Remediation/PatchRemediationButton.js
@@ -1,31 +1,16 @@
-import { Button, Flex, FlexItem, Spinner } from '@patternfly/react-core';
-import { AnsibleTowerIcon } from '@patternfly/react-icons';
-import globalPaletteWhite from '@patternfly/react-tokens/dist/js/global_palette_white';
+import { Button } from '@patternfly/react-core';
 import propTypes from 'prop-types';
 import React from 'react';
-import messages from '../../Messages';
-import { intl } from '../../Utilities/IntlProvider';
 
 const PatchRemediationButton = ({ onClick, isDisabled, isLoading, ouia }) => {
     return (
         <Button
             isDisabled={isDisabled}
-            className={'remediationButtonPatch'}
             onClick={onClick}
             ouiaId={ouia}
-            isSmall
-        ><Flex flex={{ default: 'inlineFlex' }}
-                alignItems={{ default: 'alignItemsCenter' }}
-                justifyContent={{ default: 'justifyContentCenter' }}>
-                <FlexItem spacer={{ default: 'spacerXs' }} style={{ display: 'flex' }}>
-                    {isLoading &&
-                                                <Spinner isSVG size='md'/>
-                                     || <AnsibleTowerIcon color={globalPaletteWhite.value}/>}
-                </FlexItem>
-                <FlexItem spacer={{ default: 'spacerXs' }} style={{ display: 'flex' }}>
-                                     &nbsp;{intl.formatMessage(messages.labelsRemediate)}
-                </FlexItem>
-            </Flex>
+            variant="primary"
+            isLoading={isLoading}
+        >Remediate
         </Button>
     );
 };
