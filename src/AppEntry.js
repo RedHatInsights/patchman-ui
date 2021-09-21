116a91fd1fd8e83b355c476d1ed54dcc774b450e
mkholjur <mkholjur@redhat.com>
2021-09-21 09:24:46 +0200

    chore(AppEntry): remove obsolete code

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
@@ -7,28 +7,15 @@ import getBaseName from './Utilities/getBaseName';
 import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
 import messages from '../locales/en.json';
 
-const Patchman = () => {
-    useEffect(() => {
-        /**
-         * Temporary overflow fix for chrome 1. There is no risk breaking styles for other apps, no SPA
-         * Can be deleted once chrome 2 is deployed everywhere. Overflow is fixed by "patch-root" class in chrome 2
-         */
-        const elem = document.querySelector('main#root');
-        if (!window.insights.chrome.isChrome2 && elem) {
-            elem.style.overflow = 'inherit';
-        }
-    }, []);
-    return (
-        <div className="patch-root">
-            <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages}>
-                <Provider store={getStore()}>
-                    <Router basename={getBaseName(window.location.pathname)}>
-                        <App style={{ overflow: 'clip', background: 'red', 'z-index': '1000' }} />
-                    </Router>
-                </Provider>
-            </IntlProvider>
-        </div>
-    );
-};
+const Patchman = () => (<div className="patch-root">
+    <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages}>
+        <Provider store={getStore()}>
+            <Router basename={getBaseName(window.location.pathname)}>
+                <App style={{ overflow: 'clip', background: 'red', 'z-index': '1000' }} />
+            </Router>
+        </Provider>
+    </IntlProvider>
+</div>
+);
 
 export default Patchman;
