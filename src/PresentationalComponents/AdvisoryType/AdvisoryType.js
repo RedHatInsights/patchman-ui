c2d1f669df10a67becb1464c50873c1fafd204e6
Jiri Dostal <jdostal@redhat.com>
2021-10-06 15:52:35 +0200

    fix(advisory_type): SPM-1199 fix advisory type in frontend

--- src/PresentationalComponents/AdvisoryType/AdvisoryType.js
+++ src/PresentationalComponents/AdvisoryType/AdvisoryType.js
@@ -4,9 +4,9 @@ import { advisoryTypes } from '../../Utilities/constants';
 import { Flex, FlexItem } from '@patternfly/react-core';
 
 const AdvisoryType = ({ type }) => {
-    console.log(type);
     const advisoryType =
-        advisoryTypes.find(item => item.value === type);
+        advisoryTypes.find(item => item.value === type) || advisoryTypes[3];
+    console.log(advisoryType);
     return (
         <Flex flex={{ default: 'inlineFlex' }} style={{ flexWrap: 'nowrap' }}>
             <FlexItem>{advisoryType.icon}</FlexItem>
