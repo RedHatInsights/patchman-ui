3d8299b237779ac2536e58f6e35250c1d9271484
mkholjur <mkholjur@redhat.com>
2021-10-19 21:36:05 +0200

    fix(PackageHeader): SPM-1210 fix title bottom space

--- src/PresentationalComponents/PackageHeader/PackageHeader.js
+++ src/PresentationalComponents/PackageHeader/PackageHeader.js
@@ -1,33 +1,31 @@
 import { Grid, GridItem, Stack, StackItem } from '@patternfly/react-core';
 import propTypes from 'prop-types';
 import React from 'react';
+import { Fragment } from 'react';
 import { truncateDescription } from '../../Utilities/Helpers';
 import WithLoader, { WithLoaderVariants } from '../WithLoader/WithLoader';
 
-const PackageHeader = ({ attributes, isLoading }) => {
+const PackageHeader = ({ attributes: { description }, isLoading }) => {
     const [wordLength, setWordLength] = React.useState(1000);
 
-    return (
-        <Grid hasGutter style={{ minHeight: 50 }}>
-            <GridItem md={8} sm={12}>
-                <WithLoader
-                    loading={isLoading}
-                    variant={WithLoaderVariants.spinner}
-                    centered
-                >
-                    <Stack hasGutter>
-                        <StackItem />
-                        <StackItem style={{ whiteSpace: 'pre-line' }}>
-                            {
-                                attributes.description && truncateDescription(attributes.description, wordLength, setWordLength)
-                            }
-                        </StackItem>
-                    </Stack>
-                </WithLoader>
-            </GridItem>
-
-        </Grid>
-    );
+    return description && (<Grid hasGutter style={{ minHeight: 50 }}>
+        <GridItem md={8} sm={12}>
+            <WithLoader
+                loading={isLoading}
+                variant={WithLoaderVariants.spinner}
+                centered
+            >
+                <Stack hasGutter>
+                    <StackItem />
+                    <StackItem style={{ whiteSpace: 'pre-line' }}>
+                        {
+                            description && truncateDescription(description, wordLength, setWordLength)
+                        }
+                    </StackItem>
+                </Stack>
+            </WithLoader>
+        </GridItem>
+    </Grid>) || <Fragment/>;
 };
 
 PackageHeader.propTypes = {
