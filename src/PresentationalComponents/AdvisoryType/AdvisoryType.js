4e2c3b0c58c8a95a3b0f4cd1769d24c6633a187d
mkholjur <mkholjur@redhat.com>
2022-01-31 10:58:15 +0100

    SPM-1321: make advisory cards responsive

--- src/PresentationalComponents/AdvisoryType/AdvisoryType.js
+++ src/PresentationalComponents/AdvisoryType/AdvisoryType.js
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { advisoryTypes } from '../../Utilities/constants';
-import { Flex, FlexItem } from '@patternfly/react-core';
+import { Split, SplitItem } from '@patternfly/react-core';
 
 const AdvisoryType = ({ type }) => {
     const advisoryType =
         advisoryTypes.find(item => item.value === type) || advisoryTypes[3];
     return (
-        <Flex flex={{ default: 'inlineFlex' }} style={{ flexWrap: 'nowrap' }}>
-            <FlexItem>{advisoryType.icon}</FlexItem>
-            <FlexItem isFilled>{advisoryType.label}</FlexItem>
-        </Flex>
+        <Split hasGutter>
+            <SplitItem>{advisoryType.icon}</SplitItem>
+            <SplitItem isFilled>{advisoryType.label}</SplitItem>
+        </Split>
     );
 };
 
