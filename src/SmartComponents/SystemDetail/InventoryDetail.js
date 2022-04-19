68af2b33d10cf5ac9cc84cfb5712914e24bc781d
mkholjur <mkholjur@redhat.com>
2022-04-19 23:43:44 +0200

    feat(SPM-1311): assign systems to a patch-set in System details page

--- src/SmartComponents/SystemDetail/InventoryDetail.js
+++ src/SmartComponents/SystemDetail/InventoryDetail.js
@@ -7,14 +7,24 @@ import { register } from '../../store';
 import { SystemDetailStore } from '../../store/Reducers/SystemDetailStore';
 import { intl } from '../../Utilities/IntlProvider';
 import messages from '../../Messages';
-import { setPageTitle } from '../../Utilities/Hooks';
+import { setPageTitle, useFeatureFlag } from '../../Utilities/Hooks';
 import { InventoryDetailHead, AppInfo, DetailWrapper } from '@redhat-cloud-services/frontend-components/Inventory';
 import { Alert } from '@patternfly/react-core';
 import { fetchSystemDetailsAction } from '../../store/Actions/Actions';
 import propTypes from 'prop-types';
 import { clearNotifications } from '@redhat-cloud-services/frontend-components-notifications/redux';
+import { featureFlags } from '../../Utilities/constants';
+import PatchSetWizard from '../PatchSetWizard/PatchSetWizard';
 
 const InventoryDetail = ({ match }) => {
+    const isPatchSetEnabled = useFeatureFlag(featureFlags.patch_set);
+
+    const [patchSetState, setBaselineState] = React.useState({
+        isOpen: false,
+        shouldRefresh: false,
+        systemsIDs: []
+    });
+
     const dispatch = useDispatch();
     const entityDetails = useSelector(
         ({ entityDetails }) => entityDetails && entityDetails.entity
@@ -34,6 +44,10 @@ const InventoryDetail = ({ match }) => {
     const pageTitle = entityDetails && `${entityDetails.display_name} - ${intl.formatMessage(messages.titlesSystems)}`;
     setPageTitle(pageTitle);
 
+    const showBaselineModal = () => {
+        setBaselineState({ isOpen: true, systemsIDs: [entityId] });
+    };
+
     return (
         <DetailWrapper
             onLoad={({ mergeWithDetail }) => {
@@ -42,6 +56,8 @@ const InventoryDetail = ({ match }) => {
                 });
             }}
         >
+            {(patchSetState.isOpen && isPatchSetEnabled) &&
+                <PatchSetWizard systemsIDs={patchSetState.systemsIDs} setBaselineState={setBaselineState} />}
             <Header
                 title=""
                 headerOUIA={'inventory-details'}
@@ -57,7 +73,13 @@ const InventoryDetail = ({ match }) => {
                     }
                 ]}
             >
-                <InventoryDetailHead hideBack>
+                <InventoryDetailHead hideBack actions={isPatchSetEnabled && [
+                    {
+                        title: intl.formatMessage(messages.titlesPatchSetAssignMultipleButton),
+                        key: 'assign-to-patch-set',
+                        onClick: showBaselineModal
+                    }]}
+                >
                     { hasThirdPartyRepo &&
                         (<Alert className='pf-u-mt-md' isInline variant="info"
                             title={intl.formatMessage(messages.textThirdPartyInfo)}>
--- src/SmartComponents/SystemDetail/InventoryDetail.test.js
+++ src/SmartComponents/SystemDetail/InventoryDetail.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import InventoryDetail from './InventoryDetail';
 import toJson from 'enzyme-to-json';
 import { Provider } from 'react-redux';
@@ -7,13 +6,13 @@ import configureStore from 'redux-mock-store';
 import { initMocks } from '../../Utilities/unitTestingUtilities.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useFeatureFlag } from '../../Utilities/Hooks';
 
 initMocks();
 
-jest.mock('../../store', () => ({
-    ...jest.requireActual('../../store'),
-    getStore: jest.fn(),
-    register: jest.fn()
+jest.mock('../../Utilities/Hooks', () => ({
+    ...jest.requireActual('../../Utilities/Hooks'),
+    useFeatureFlag: jest.fn()
 }));
 
 jest.mock('react-redux', () => ({
@@ -23,10 +22,10 @@ jest.mock('react-redux', () => ({
 
 jest.mock('@redhat-cloud-services/frontend-components/Inventory', () => ({
     ...jest.requireActual('@redhat-cloud-services/frontend-components/Inventory'),
-    InventoryTable: jest.fn(() => <div className='testInventroyComponentChild'><div>This is child</div></div>),
-    InventoryDetailHead: jest.fn(() => <div className='testInventoryDetailHeadChild'><div>This is child</div></div>),
+    InventoryTable: jest.fn(() => <div className='testInventroyComponent'><div>This is child</div></div>),
+    InventoryDetailHead: jest.fn(() => <div className='testInventoryDetailHead'><div>This is child</div></div>),
     AppInfo: jest.fn(() => <div className='testInventroyAppInfo'><div>This is child</div></div>),
-    DetailWrapper: jest.fn(() => <div className='testDetailWrapperChild'><div>This is child</div></div>)
+    DetailWrapper: jest.fn(({ children }) => <div className='testDetailWrapper'><div>{children}</div></div>)
 }));
 
 const mockState = { ...entityDetail };
@@ -64,8 +63,36 @@ afterEach(() => {
 
 describe('InventoryPage.js', () => {
     it('Should match the snapshots', () => {
-        console.log(wrapper.debug());
-        //expect(toJson(wrapper.update())).toMatchSnapshot();
+        expect(toJson(wrapper.update())).toMatchSnapshot();
+    });
+
+    describe('test patch-set: ', () => {
+        it('Should hide actions dropdown for patch-set when flag is disabled', () => {
+            useFeatureFlag.mockReturnValue(false);
+
+            const tempWrapper = mount(<Provider store={store}>
+                <Router><InventoryDetail match={{ params: { inventoryId: 'test' } }} /></Router>
+            </Provider>);
+
+            const { actions } = tempWrapper.find('.testInventoryDetailHead').parent().props();
+
+            expect(actions).toBeFalsy();
+            expect(tempWrapper.update().find('.patch-set').length).toBe(0);
+        });
+
+        it('Should show actions dropdown for patch-set when flag is enabled', () => {
+            useFeatureFlag.mockReturnValue(true);
+
+            const tempWrapper = mount(<Provider store={store}>
+                <Router><InventoryDetail match={{ params: { inventoryId: 'test' } }} /></Router>
+            </Provider>);
+
+            const { actions } = tempWrapper.find('.testInventoryDetailHead').parent().props();
+            actions[0].onClick();
+
+            expect(actions.length).toEqual(1);
+            expect(tempWrapper.update().find('.patch-set').length).toBe(2);
+        });
     });
 });
 
