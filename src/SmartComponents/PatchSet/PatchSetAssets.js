d130c6b007341ecace06d62b56a7b1155747d879
Adonis Puente <adonis.puente@gmail.com>
2022-11-09 16:04:02 -0500

    Minor Suggested changes

--- src/SmartComponents/PatchSet/PatchSetAssets.js
+++ src/SmartComponents/PatchSet/PatchSetAssets.js
@@ -7,7 +7,7 @@ import {
 import { intl } from '../../Utilities/IntlProvider';
 import messages from '../../Messages';
 
-export const createPatchSetButton = (setPatchSetState, hasAccess) => () =>
+export const CreatePatchSetButton = (setPatchSetState, hasAccess) => () =>
     !hasAccess ?
         <Tooltip content='For editing access, contact your administrator.'>
             <Button key='createButton' isAriaDisabled >
@@ -54,7 +54,7 @@ export const patchSetRowActions = (openPatchSetEditModal, handlePatchSetDelete) 
 
 ];
 
-export const customActionsToggle = () => <Tooltip content='For editing access, contact your administrator.'>
+export const CustomActionsToggle = () => <Tooltip content='For editing access, contact your administrator.'>
     <Button
         isAriaDisabled
         variant="plain"
