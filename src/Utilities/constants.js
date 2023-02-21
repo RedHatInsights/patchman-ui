31c17ea51c063ac1bbc162748511243b0da78505
Ryan Deussing <ryandeussing@gmail.com>
2023-02-21 13:01:38 -0500

    update docs versions for Insights, Console, Cost & Subscriptions

--- src/Utilities/constants.js
+++ src/Utilities/constants.js
@@ -1,289 +1,293 @@
-import BugIcon from '@patternfly/react-icons/dist/js/icons/bug-icon';
-import EnhancementIcon from '@patternfly/react-icons/dist/js/icons/enhancement-icon';
-import SecurityIcon from '@patternfly/react-icons/dist/js/icons/security-icon';
-import FlagIcon from '@patternfly/react-icons/dist/js/icons/flag-icon';
-import React from 'react';
-import { subtractDate } from './Helpers';
+import BugIcon from "@patternfly/react-icons/dist/js/icons/bug-icon";
+import EnhancementIcon from "@patternfly/react-icons/dist/js/icons/enhancement-icon";
+import SecurityIcon from "@patternfly/react-icons/dist/js/icons/security-icon";
+import FlagIcon from "@patternfly/react-icons/dist/js/icons/flag-icon";
+import React from "react";
+import { subtractDate } from "./Helpers";
 
-export const STATUS_REJECTED = 'rejected';
-export const STATUS_LOADING = 'loading';
-export const STATUS_RESOLVED = 'resolved';
+export const STATUS_REJECTED = "rejected";
+export const STATUS_LOADING = "loading";
+export const STATUS_RESOLVED = "resolved";
 
 export const compoundSortValues = {
     operating_system: {
-        asc: 'osname,osmajor,osminor',
-        desc: '-osname,-osmajor,-osminor'
+        asc: "osname,osmajor,osminor",
+        desc: "-osname,-osmajor,-osminor",
     },
     applicable_advisories: {
-        asc: 'rhsa_count,rhba_count,rhea_count',
-        desc: '-rhsa_count,-rhba_count,-rhea_count'
-    }
+        asc: "rhsa_count,rhba_count,rhea_count",
+        desc: "-rhsa_count,-rhba_count,-rhea_count",
+    },
 };
 
 // messy check because of frontend-components tests
-export const ENABLE_PACKAGES = window?.insights?.chrome?.isBeta && insights?.chrome?.isBeta();
+export const ENABLE_PACKAGES =
+    window?.insights?.chrome?.isBeta && insights?.chrome?.isBeta();
 export const storeListDefaults = {
     rows: [],
     status: { isLoading: true },
     metadata: {
         limit: 25,
         offset: 0,
-        total_items: 0
+        total_items: 0,
     },
     expandedRows: {},
     selectedRows: {},
     queryParams: {
         page: 1,
-        page_size: 20
+        page_size: 20,
     },
-    error: {}
+    error: {},
 };
 
 export const systemPackagesDefaultFilters = {
-    filter: { updatable: ['true'] }
+    filter: { updatable: ["true"] },
 };
 
 export const packagesListDefaultFilters = {
-    filter: { systems_updatable: ['gt:0'] }
+    filter: { systems_updatable: ["gt:0"] },
 };
 
 export const systemsListDefaultFilters = {
-    filter: { stale: [true, false] }
+    filter: { stale: [true, false] },
 };
 
 export const publicDateOptions = [
-    { apiValue: '', label: 'All', value: 'all' },
+    { apiValue: "", label: "All", value: "all" },
     {
         apiValue: `gt:${subtractDate(7)}`,
-        label: 'Last 7 days',
-        value: 'last7'
+        label: "Last 7 days",
+        value: "last7",
     },
     {
         apiValue: `gt:${subtractDate(30)}`,
-        label: 'Last 30 days',
-        value: 'last30'
+        label: "Last 30 days",
+        value: "last30",
     },
     {
         apiValue: `gt:${subtractDate(90)}`,
-        label: 'Last 90 days',
-        value: 'last90'
+        label: "Last 90 days",
+        value: "last90",
     },
     {
         apiValue: `gt:${subtractDate(365)}`,
-        label: 'Last year',
-        value: 'lastYear'
+        label: "Last year",
+        value: "lastYear",
     },
     {
         apiValue: `lt:${subtractDate(365)}`,
-        label: 'More than 1 year ago',
-        value: 'moreThanYear'
-    }
+        label: "More than 1 year ago",
+        value: "moreThanYear",
+    },
 ];
 
 export const advisorySeverities = [
     {
         value: 0,
-        label: 'N/A',
-        color: 'var(--pf-global--Color--200)'
+        label: "N/A",
+        color: "var(--pf-global--Color--200)",
     },
     {
         value: 1,
-        label: 'Low',
-        color: 'var(--pf-global--Color--200)',
+        label: "Low",
+        color: "var(--pf-global--Color--200)",
         text:
-            'This rating is given to all other issues that have a security impact. These are the types of vulnerabilities' +
-            ' that are believed to require unlikely circumstances to be able to be exploited, or where a successful exploit' +
-            ' would give minimal consequences.'
+            "This rating is given to all other issues that have a security impact. These are the types of vulnerabilities" +
+            " that are believed to require unlikely circumstances to be able to be exploited, or where a successful exploit" +
+            " would give minimal consequences.",
     },
     {
         value: 2,
-        label: 'Moderate',
-        color: 'var(--pf-global--warning-color--100)',
+        label: "Moderate",
+        color: "var(--pf-global--warning-color--100)",
         text:
-            'This rating is given to flaws that may be more difficult to exploit but could still lead to some' +
-            ' compromise of the confidentiality, integrity, or availability of resources, under certain circumstances.' +
-            ' These are the types of vulnerabilities that could have had a Critical impact or Important impact' +
-            ' but are less easily exploited based on a technical evaluation of the flaw, or affect unlikely' +
-            ' configurations.'
+            "This rating is given to flaws that may be more difficult to exploit but could still lead to some" +
+            " compromise of the confidentiality, integrity, or availability of resources, under certain circumstances." +
+            " These are the types of vulnerabilities that could have had a Critical impact or Important impact" +
+            " but are less easily exploited based on a technical evaluation of the flaw, or affect unlikely" +
+            " configurations.",
     },
     {
         value: 3,
-        label: 'Important',
-        color: '#ec7a08',
+        label: "Important",
+        color: "#ec7a08",
         text:
-            'This rating is given to flaws that can easily compromise the confidentiality, integrity, or availability' +
-            ' of resources. These are the types of vulnerabilities that allow local users to gain privileges, allow' +
-            ' unauthenticated remote users to view resources that should otherwise be protected by authentication,' +
-            ' allow authenticated remote users to execute arbitrary code, or allow remote users to cause a denial' +
-            ' of service.'
+            "This rating is given to flaws that can easily compromise the confidentiality, integrity, or availability" +
+            " of resources. These are the types of vulnerabilities that allow local users to gain privileges, allow" +
+            " unauthenticated remote users to view resources that should otherwise be protected by authentication," +
+            " allow authenticated remote users to execute arbitrary code, or allow remote users to cause a denial" +
+            " of service.",
     },
     {
         value: 4,
-        label: 'Critical',
-        color: 'var(--pf-global--danger-color--100)',
+        label: "Critical",
+        color: "var(--pf-global--danger-color--100)",
         text:
-            'This rating is given to flaws that could be easily exploited by a remote unauthenticated attacker' +
-            ' and lead to system compromise (arbitrary code execution) without requiring user interaction.' +
-            ' These are the types of vulnerabilities that can be exploited by worms. Flaws that require an authenticated' +
-            ' remote user, a local user, or an unlikely configuration are not classed as Critical impact.'
-    }
+            "This rating is given to flaws that could be easily exploited by a remote unauthenticated attacker" +
+            " and lead to system compromise (arbitrary code execution) without requiring user interaction." +
+            " These are the types of vulnerabilities that can be exploited by worms. Flaws that require an authenticated" +
+            " remote user, a local user, or an unlikely configuration are not classed as Critical impact.",
+    },
 ];
 
 export const advisoryTypes = [
     {
-        value: 'security',
-        label: 'Security',
-        icon: <SecurityIcon />
+        value: "security",
+        label: "Security",
+        icon: <SecurityIcon />,
     },
     {
-        value: 'bugfix',
-        label: 'Bugfix',
-        icon: <BugIcon />
+        value: "bugfix",
+        label: "Bugfix",
+        icon: <BugIcon />,
     },
     {
-        value: 'enhancement',
-        label: 'Enhancement',
-        icon: <EnhancementIcon />
+        value: "enhancement",
+        label: "Enhancement",
+        icon: <EnhancementIcon />,
     },
     {
-        value: 'other',
-        label: 'Other',
-        icon: <FlagIcon />
-    }
+        value: "other",
+        label: "Other",
+        icon: <FlagIcon />,
+    },
 ];
 
 export const updatableTypes = [
     {
         value: false,
-        label: 'Up-to-date '
+        label: "Up-to-date ",
     },
     {
         value: true,
-        label: 'Upgradable'
-    }
+        label: "Upgradable",
+    },
 ];
 
 export const packagesListUpdatableTypes = [
     {
-        value: 'eq:0',
-        label: 'Systems up to date'
+        value: "eq:0",
+        label: "Systems up to date",
     },
     {
-        value: 'gt:0',
-        label: 'Systems with patches available'
-    }
+        value: "gt:0",
+        label: "Systems with patches available",
+    },
 ];
 
 export const staleSystems = [
     {
         value: true,
-        label: 'Stale'
+        label: "Stale",
     },
     {
         value: false,
-        label: 'Fresh'
-    }
+        label: "Fresh",
+    },
 ];
 
 export const rebootRequired = [
     {
         value: true,
-        label: 'Required'
+        label: "Required",
     },
     {
         value: false,
-        label: 'Not required'
-    }
+        label: "Not required",
+    },
 ];
 
 export const filterCategories = {
     advisory_type_name: {
-        label: 'Advisory type',
-        values: advisoryTypes
+        label: "Advisory type",
+        values: advisoryTypes,
     },
     public_date: {
-        label: 'Public date',
-        values: publicDateOptions
+        label: "Public date",
+        values: publicDateOptions,
     },
     updatable: {
-        label: 'Status',
-        values: updatableTypes
+        label: "Status",
+        values: updatableTypes,
     },
     systems_updatable: {
-        label: 'Status',
-        values: packagesListUpdatableTypes
+        label: "Status",
+        values: packagesListUpdatableTypes,
     },
     packages_updatable: {
-        label: 'Patch status',
-        values: packagesListUpdatableTypes
+        label: "Patch status",
+        values: packagesListUpdatableTypes,
     },
     stale: {
-        label: 'Status',
-        values: staleSystems
+        label: "Status",
+        values: staleSystems,
     },
     reboot_required: {
-        label: 'Reboot required',
-        values: rebootRequired
+        label: "Reboot required",
+        values: rebootRequired,
     },
     os: {
-        label: 'Operating system'
-    }
+        label: "Operating system",
+    },
 };
 
 export const entityTypes = {
-    advisories: 'advisories',
-    packages: 'packages'
+    advisories: "advisories",
+    packages: "packages",
 };
 
 export const ReadOnlyNotification = {
-    title: 'title',
-    detail: 'message'
+    title: "title",
+    detail: "message",
 };
 
 export const remediationIdentifiers = {
-    package: 'patch-package',
-    advisory: 'patch-advisory'
+    package: "patch-package",
+    advisory: "patch-advisory",
 };
 
 export const exportNotifications = (format) => ({
     pending: {
         title: `Preparing export of ${format?.toUpperCase()} format. Once complete, your download will start automatically.`,
-        variant: 'info'
+        variant: "info",
     },
     success: {
         title: `The exported ${format?.toUpperCase()} file is being downloaded`,
-        variant: 'success'
+        variant: "success",
     },
     error: {
-        title: 'Couldn’t download export. Reinitiate this export to try again.',
-        variant: 'danger'
-    }
+        title: "Couldn’t download export. Reinitiate this export to try again.",
+        variant: "danger",
+    },
 });
 
-export const patchSetDeleteNotifications = ({
+export const patchSetDeleteNotifications = {
     success: {
         title: `Patch template has been deleted.`,
-        variant: 'success'
+        variant: "success",
     },
     error: {
-        title: 'Couldn’t delete patch template.',
-        variant: 'danger'
-    }
-});
+        title: "Couldn’t delete patch template.",
+        variant: "danger",
+    },
+};
 
-export const multiValueFilters = ['installed_evra', 'os'];
+export const multiValueFilters = ["installed_evra", "os"];
 
 export const featureFlags = {
-    patch_set: 'patch.patch_set'
+    patch_set: "patch.patch_set",
 };
 
 export const patchSetUnassignSystemsNotifications = (systemsCount) => ({
     success: {
         title: `Systems succesfully removed from this Patch template.`,
-        description: `${systemsCount} ${systemsCount > 1 ? 'systems' : 'system'} removed from Patch template(s)`,
-        variant: 'success'
-    }
+        description: `${systemsCount} ${
+            systemsCount > 1 ? "systems" : "system"
+        } removed from Patch template(s)`,
+        variant: "success",
+    },
 });
 
-export const TEMPLATES_DOCS_LINK = 'https://access.redhat.com/documentation/en-us/red_hat_insights/2022/html/'
-    + 'system_patching_using_ansible_playbooks_via_remediations/index';
+export const TEMPLATES_DOCS_LINK =
+    "https://access.redhat.com/documentation/en-us/red_hat_insights/2023/html/" +
+    "system_patching_using_ansible_playbooks_via_remediations/index";
