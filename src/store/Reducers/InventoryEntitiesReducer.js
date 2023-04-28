d827155474042d7d5d1c306e04ad9269790ad051
leSamo <soleksak@redhat.com>
2023-04-28 22:09:57 +0200

    Fix remediation not working in the package detail page

--- src/store/Reducers/InventoryEntitiesReducer.js
+++ src/store/Reducers/InventoryEntitiesReducer.js
@@ -31,7 +31,32 @@ export const modifyInventory = (columns, state) => {
     return state;
 };
 
-export const modifyPackageSystems = (columns, state) => {
+export const modifyPackageSystems = (columns, state, lastAction) => {
+    // this is pretty scuffed, but trust me it cannot be solved more elegantly because
+    // single item select actions are triggered by Inventory and contain only system id
+    // but we also need to match available_evra for each selected system for remediations
+    // this does not happen with bulk select action because they are implemented by Patchman
+    // the id-evra mapping is cached to minimize O(n^2) time complexity of id-evra matching
+    if (lastAction === 'SELECT_ENTITY') {
+        let idToEvraMapping = state.packageEvraCache;
+
+        if (idToEvraMapping === undefined) {
+            idToEvraMapping = {};
+            state.rows.forEach(row => idToEvraMapping[row.id] = row.available_evra);
+            state.packageEvraCache = idToEvraMapping;
+        }
+
+        state.selectedRows = state.selectedRows ? Object.fromEntries(
+            Object.entries(state.selectedRows).map(([id, value]) =>
+                value === true ? [id, idToEvraMapping[id]] : [id, value]
+            )
+        ) : state.selectedRows;
+    }
+
+    if (lastAction === 'LOAD_ENTITIES_FULFILLED') {
+        state.packageEvraCache = undefined;
+    }
+
     if (state.loaded) {
         return {
             ...state,
@@ -72,7 +97,7 @@ export const inventoryEntitiesReducer = (columns, inventoryModifier) => (state =
 
     switch (action.type) {
         case 'LOAD_ENTITIES_FULFILLED':
-            return inventoryModifier(columns, newState);
+            return inventoryModifier(columns, newState, action.type);
 
         case 'LOAD_ENTITIES_PENDING':
             newState.status = { isLoading: true, hasError: false };
@@ -83,7 +108,7 @@ export const inventoryEntitiesReducer = (columns, inventoryModifier) => (state =
 
         case 'SELECT_ENTITY': {
             const stateAfterSelection = selectRows(newState, action);
-            return inventoryModifier(columns, stateAfterSelection);
+            return inventoryModifier(columns, stateAfterSelection, action.type);
         }
 
         case ActionTypes.CLEAR_INVENTORY_REDUCER:
