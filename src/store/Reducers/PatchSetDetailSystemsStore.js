999efa94bc23865fe136b297e7f5336d39919df6
leSamo <soleksak@redhat.com>
2023-05-03 15:57:33 +0200

    Fix zero state on Template detail page

--- src/store/Reducers/PatchSetDetailSystemsStore.js
+++ src/store/Reducers/PatchSetDetailSystemsStore.js
@@ -2,7 +2,8 @@ import { changeListParams } from '../../Utilities/Helpers';
 import {
     CHANGE_PATCH_SET_DETAIL_SYSTEMS_PARAMS,
     TRIGGER_GLOBAL_FILTER,
-    CHANGE_PATCH_SET_DETAIL_SYSTEMS_METADATA
+    CHANGE_PATCH_SET_DETAIL_SYSTEMS_METADATA,
+    FETCH_PATCH_SET_SYSTEMS_NO_FILTERS
 } from '../ActionTypes';
 import { changeFilters } from './HelperReducers';
 
@@ -11,7 +12,9 @@ const initialState = {
     queryParams: {
         page: 1,
         perPage: 20
-    }
+    },
+    templateHasSystemsLoading: true,
+    templateHasSystems: null
 };
 
 export const PatchSetDetailSystemsStore = (state = initialState, action) => {
@@ -28,6 +31,28 @@ export const PatchSetDetailSystemsStore = (state = initialState, action) => {
         case CHANGE_PATCH_SET_DETAIL_SYSTEMS_METADATA:
             newState.metadata = action.payload;
             return newState;
+
+        case FETCH_PATCH_SET_SYSTEMS_NO_FILTERS + '_PENDING':
+            return {
+                ...state,
+                templateHasSystemsLoading: true,
+                templateHasSystems: null
+            };
+
+        case FETCH_PATCH_SET_SYSTEMS_NO_FILTERS + '_FULFILLED':
+            return {
+                ...state,
+                templateHasSystemsLoading: false,
+                templateHasSystems: action.payload.data.length > 0
+            };
+
+        case FETCH_PATCH_SET_SYSTEMS_NO_FILTERS + '_REJECTED':
+            return {
+                ...state,
+                templateHasSystemsLoading: false,
+                // if we set this to true, Inventory table is shown, which has better error handling
+                templateHasSystems: true
+            };
 
         default:
             return state;
