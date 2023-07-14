266cb28642f7298823cd82f03932effdab212a42
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-07-14 09:57:01 +0200

    Revert "undo temp commit, enable pushing assets with sourcemaps to sentry (#1070)"
    This reverts commit 9916b414c9a8e0abbc951263f5999158f4fe0de4.

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -5,21 +5,15 @@ set -x
 # map master branch to stage-beta environment
 if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
     echo "PUSHING stage-beta"
+
+    ls -la ./dist
+
     rm -rf ./dist/.git
     .travis/release.sh "stage-beta"
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "stage-stable" ]]; then
-    echo "PUSHING stage-stable"
-    rm -rf ./build/.git
-    .travis/release.sh "${TRAVIS_BRANCH}"
-fi
-
-if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     echo "PUSHING ${TRAVIS_BRANCH}"
     rm -rf ./build/.git
-
-    #we do not want sourmaps uploaded to browser for prod-beta and prod-stable envs
-    rm -rf ./build/sourcemaps
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
