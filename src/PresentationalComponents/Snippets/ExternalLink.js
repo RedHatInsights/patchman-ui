713a95cb8a461a3f1be58ecf7a6a784b6799bd27
Lior Keren <lkeren@redhat.com>
2023-07-31 18:43:16 +0300

    External link pattern fix - use PF4 ExternalLinkAltIcon
    Jira ticket link: https://issues.redhat.com/browse/SPM-1761

--- src/PresentationalComponents/Snippets/ExternalLink.js
+++ src/PresentationalComponents/Snippets/ExternalLink.js
@@ -1,5 +1,5 @@
 import { Flex, FlexItem } from '@patternfly/react-core';
-import ExternalLinkSquareAltIcon from '@patternfly/react-icons/dist/js/icons/external-link-square-alt-icon';
+import ExternalLinkAltIcon from '@patternfly/react-icons/dist/js/icons/external-link-alt-icon';
 import propTypes from 'prop-types';
 import React from 'react';
 
@@ -14,7 +14,7 @@ const ExternalLink = ({ link, text }) => {
                     {text}
                 </FlexItem>
                 <FlexItem spacer={{ default: 'spacerSm' }}>
-                    <ExternalLinkSquareAltIcon />
+                    <ExternalLinkAltIcon />
                 </FlexItem>
             </Flex>
         </a>
