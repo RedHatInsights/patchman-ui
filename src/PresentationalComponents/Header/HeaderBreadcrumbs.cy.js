1889393287c22a1ecb71de402288352852bca677
Aleksandr Voznesenskii <avoznese@redhat.com>
2023-09-05 13:53:58 +0200

    reverted cypress changes

--- src/PresentationalComponents/Header/HeaderBreadcrumbs.cy.js
+++ src/PresentationalComponents/Header/HeaderBreadcrumbs.cy.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from '@cypress/react';
+import { mount } from '@cypress';
 
 import HeaderBreadcrumbs from './HeaderBreadcrumbs';
 import { MemoryRouter } from 'react-router-dom';
