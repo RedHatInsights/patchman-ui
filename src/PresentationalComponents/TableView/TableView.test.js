973ab22d1ef3d58ad212c1615525f36517cbd0cd
Fewwy <avoznese@redhat.com>
2023-10-20 14:17:51 +0200

    tableview.test.js refactored

--- src/PresentationalComponents/TableView/TableView.test.js
+++ src/PresentationalComponents/TableView/TableView.test.js
@@ -1,6 +1,10 @@
 import TableView from './TableView';
-import toJson from 'enzyme-to-json';
-import NoRegisteredSystems from '../../PresentationalComponents/Snippets/NoRegisteredSystems';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Provider, useSelector } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { storeListDefaults } from '../../Utilities/constants';
+import { systemPackages } from '../../Utilities/RawDataForTesting';
+import userEvent from '@testing-library/user-event';
 
 const testObj = {
     columns: [],
@@ -32,87 +36,186 @@ const testObj = {
     selectedRows: {},
     apply: jest.fn()
 };
+const mockOnSelect = jest.fn();
+
+const initStore = (state) => {
+    const customMiddleWare = () => next => action => {
+        useSelector.mockImplementation(callback => {
+            return callback({ PackagesListStore: state });
+        });
+        next(action);
+    };
+
+    const mockStore = configureStore([customMiddleWare]);
+    return mockStore({ PackagesListStore: state });
+};
+
+const mockState = {
+    ...storeListDefaults,
+    rows: systemPackages,
+    status: { isLoading: false, code: 200, hasError: false }
+};
+
+let store = initStore(mockState);
 
 describe('TableView', () => {
     it('TableView', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const { asFragment } = render(
+            <Provider store={store}>
+                <TableView {...testObj} />
+            </Provider>
+        );
+        expect(asFragment()).toMatchSnapshot();
     });
 
     describe('test table props', () => {
-        const wrapper = shallow(<TableView {...testObj}  store = {{
-            rows: [],
-            metadata: { total_items: 10 },
-            status: 'resolved',
-            queryParams: {}
-        }} />);
 
         it('TableView', () => {
-            expect(toJson(wrapper)).toMatchSnapshot();
+            const { asFragment } = render(
+                <Provider store={store}>
+                    <TableView {...testObj}  store = {{
+                        rows: [],
+                        metadata: { total_items: 10 },
+                        status: 'resolved',
+                        queryParams: {}
+                    }} />
+                </Provider>
+            );
+            expect(asFragment()).toMatchSnapshot();
         });
 
-        it('Should call onCollapse', () => {
-            const { cells }  = wrapper.find('Table').props();
-            wrapper.find('Table').props().onSelect();
-            expect(testObj.onSelect).toHaveBeenCalled();
-            expect(cells).toEqual([]);
+        it('Should call onSelect', async () => {
+            render(
+                <Provider store={store}>
+                    <TableView {...testObj}
+                        onSelect={mockOnSelect}
+                        store = {{
+                            rows: [],
+                            metadata: { total_items: 10 },
+                            status: 'resolved',
+                            queryParams: {}
+                        }} />
+                </Provider>
+            );
+            await userEvent.click(screen.getByRole('checkbox'));
+            await waitFor(() =>
+                expect(mockOnSelect).toHaveBeenCalled());
         });
 
     });
 
     it('TableView', () => {
-        const wrapper = shallow(<TableView {...testObj}  store = {{
-            rows: [],
-            metadata: {},
-            status: 'loading',
-            queryParams: {}
-        }} />);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const { asFragment } = render(
+            <Provider store={store}>
+                <TableView {...testObj}  store = {{
+                    rows: [],
+                    metadata: {},
+                    status: 'loading',
+                    queryParams: {}
+                }} />
+            </Provider>
+        );
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('Should open remediation modal', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        const { actionsConfig: { actions } }  = wrapper.find('PrimaryToolbar').props();
-        expect(actions[0]).toMatchSnapshot();
+        const { asFragment } = render(
+            <Provider store={store}>
+                <TableView {...testObj} />
+            </Provider>
+        );
+        expect(asFragment()).toMatchSnapshot();
     });
 
-    it('Should unselect', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        const { bulkSelect: { items } }  = wrapper.find('PrimaryToolbar').props();
-        items[0].onClick();
-        expect(testObj.onSelect).toHaveBeenCalledWith('none');
+    it('Should unselect', async () => {
+        await render(
+            <Provider store={store}>
+                <TableView {...testObj}
+                    onSelect={mockOnSelect}
+                    store = {{
+                        rows: [],
+                        metadata: { total_items: 10 },
+                        status: 'resolved',
+                        queryParams: {}
+                    }} />
+            </Provider>
+        );
+        await userEvent.click(screen.getByRole('button', { name: 'Select' }));
+        await userEvent.click(screen.getByRole('menuitem', { name: /select none \(0\)/i }));
+        await waitFor(() =>
+            expect(mockOnSelect).toHaveBeenCalledWith('none'));
     });
 
-    it('Should select page', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        const { bulkSelect: { items } }  = wrapper.find('PrimaryToolbar').props();
-        items[1].onClick();
-        expect(testObj.onSelect).toHaveBeenCalledWith('page');
+    it('Should select page', async () => {
+        await render(
+            <Provider store={store}>
+                <TableView {...testObj}
+                    onSelect={mockOnSelect}
+                    store = {{
+                        rows: [],
+                        metadata: { total_items: 10 },
+                        status: 'resolved',
+                        queryParams: {}
+                    }} />
+            </Provider>
+        );
+        await userEvent.click(screen.getByRole('button', { name: 'Select' }));
+        await userEvent.click(screen.getByRole('menuitem', { name: /select page \(0\)/i }));
+        await waitFor(() =>
+            expect(mockOnSelect).toHaveBeenCalledWith('page'));
     });
 
-    it('Should select all', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        const { bulkSelect: { items } }  = wrapper.find('PrimaryToolbar').props();
-        items[2].onClick();
-        expect(testObj.onSelect).toHaveBeenCalledWith('all', null, null, expect.any(Function));
+    it('Should select all', async () => {
+        await render(
+            <Provider store={store}>
+                <TableView {...testObj}
+                    onSelect={mockOnSelect}
+                    store = {{
+                        rows: [],
+                        metadata: { total_items: 10 },
+                        status: 'resolved',
+                        queryParams: {}
+                    }} />
+            </Provider>
+        );
+        await userEvent.click(screen.getByRole('button', { name: 'Select' }));
+        await userEvent.click(screen.getByRole('menuitem', { name: /select all \(10\)/i }));
+        await waitFor(() =>
+            expect(mockOnSelect).toHaveBeenCalledWith('all', null, null, expect.any(Function)));
     });
 
-    it('Should unselect all ', () => {
-        const wrapper = shallow(<TableView {...testObj} />);
-        const { bulkSelect: { onSelect } }  = wrapper.find('PrimaryToolbar').props();
-        onSelect(false);
-        expect(testObj.onSelect).toHaveBeenCalledWith('none');
+    it('Should unselect all ', async () => {
+        await render(
+            <Provider store={store}>
+                <TableView {...testObj}
+                    onSelect={mockOnSelect}
+                    store = {{
+                        rows: [],
+                        metadata: { total_items: 10 },
+                        status: 'resolved',
+                        queryParams: {}
+                    }} />
+            </Provider>
+        );
+        await userEvent.click(screen.getByRole('button', { name: 'Select' }));
+        await userEvent.click(screen.getByRole('menuitem', { name: /select none \(0\)/i }));
+        await waitFor(() =>
+            expect(mockOnSelect).toHaveBeenCalledWith('all', null, null, expect.any(Function)));
     });
 
-    it('Should display NoRegisteredSystems when there are no registered systems', () => {
-        const wrapper = shallow(<TableView {...testObj} store={{
-            rows: [],
-            metadata: { total_items: 10, has_systems: false },
-            status: 'resolved',
-            queryParams: {}
-        }} />);
-
-        expect(wrapper.find(NoRegisteredSystems)).toBeTruthy();
+    it('Should display NoRegisteredSystems when there are no registered systems', async () => {
+        await render(
+            <Provider store={store}>
+                <TableView {...testObj} store={{
+                    rows: [],
+                    metadata: { total_items: 10, has_systems: false },
+                    status: 'resolved',
+                    queryParams: {}
+                }} />
+            </Provider>
+        );
+        // eslint-disable-next-line max-len
+        expect(screen.getByText(/connect your systems to keep your red hat environment running efficiently, with security and compliance with various standards\./i)).toBeTruthy();
+        expect(screen.getByRole('link', { name: /learn more about connecting your systems/i })).toBeTruthy();
     });
 });
-
