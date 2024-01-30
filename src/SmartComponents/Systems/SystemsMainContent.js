a52910f96f47edc0ca2d75d8c7bc50f467083ca4
Muslimjon <mkholjur@redhat.com>
2024-01-30 13:12:15 +0100

    refactor(Systems): remove page title update from SystemsMainContent

--- src/SmartComponents/Systems/SystemsMainContent.js
+++ src/SmartComponents/Systems/SystemsMainContent.js
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import { useSelector, useDispatch } from 'react-redux';
-import messages from '../../Messages';
 import ErrorHandler from '../../PresentationalComponents/Snippets/ErrorHandler';
-import { DEFAULT_PATCH_TITLE } from '../../Utilities/constants';
 import { changeSystemsParams, clearInventoryReducer } from '../../store/Actions/Actions';
 import { usePatchSetState } from '../../Utilities/hooks';
-import { intl } from '../../Utilities/IntlProvider';
 import { decodeQueryparams } from '../../Utilities/Helpers';
 import { useActivateRemediationModal } from './SystemsListAssets';
 import SystemsStatusReport from '../../PresentationalComponents/StatusReports/SystemsStatusReport';
 import PatchSetWrapper from '../../PresentationalComponents/PatchSetWrapper/PatchSetWrapper';
-import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import RemediationWizard from '../Remediation/RemediationWizard';
 import SystemsTable from './SystemsTable';
 import { useSearchParams } from 'react-router-dom';
 
 const SystemsMainContent = () => {
-    const chrome = useChrome();
-    useEffect(()=>{
-        chrome.updateDocumentTitle(`${intl.formatMessage(messages.titlesSystems)}${DEFAULT_PATCH_TITLE}`);
-    }, [chrome, intl]);
-
     const [searchParams, setSearchParams] = useSearchParams();
     const dispatch = useDispatch();
     const [isRemediationOpen, setRemediationOpen] = useState(false);
