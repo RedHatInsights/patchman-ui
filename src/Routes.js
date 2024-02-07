4c50eb1987d0bfec8591d37004602ba25e269665
Adonis Puente <adonis.puente@gmail.com>
2024-02-07 15:21:59 -0500

    chore(RHINENG-5923): Update zero state check

--- src/Routes.js
+++ src/Routes.js
@@ -76,11 +76,12 @@ const PatchRoutes = () => {
     const generalPermissions = ['patch:*:*', 'patch:*:read'];
     const [hasSystems, setHasSystems] = useState(true);
     const INVENTORY_TOTAL_FETCH_URL = '/api/inventory/v1/hosts';
+    const RHEL_ONLY_FILTER = '?filter[system_profile][operating_system][RHEL][version][gte]=0';
 
     useEffect(() => {
         try {
             axios
-            .get(`${INVENTORY_TOTAL_FETCH_URL}?page=1&per_page=1`)
+            .get(`${INVENTORY_TOTAL_FETCH_URL}${RHEL_ONLY_FILTER}&page=1&per_page=1`)
             .then(({ data }) => {
                 setHasSystems(data.total > 0);
             });
@@ -98,37 +99,37 @@ const PatchRoutes = () => {
             }
         >
             <Routes>
-                {!hasSystems ? (
-                    <Route path='*' element={
-                        <AsyncComponent
-                            appId="content_management_zero_state"
-                            appName="dashboard"
-                            module="./AppZeroState"
-                            scope="dashboard"
-                            ErrorComponent={<div>Error state</div>}
-                            app="Content_management"
-                        />
-                    } />
-                )
-                    : (
-                        <Route element={<PermissionRoute requiredPermissions={generalPermissions} />}>
-                            <Route path='/advisories' element={<Advisories />} />
-                            <Route path='/advisories/:advisoryId/:inventoryId'
-                                element={<NavigateToSystem />} />
-                            <Route path='/advisories/:advisoryId' element={<AdvisoryPage />} />
-                            <Route path='/systems' element={<Systems />} />
-                            <Route path='/systems/:inventoryId' element={<InventoryDetail />} />
-                            <Route path='/packages' element={<PackagesPage />} />
-                            <Route path='/packages/:packageName' element={<PackageDetail />} />
-                            <Route path='/packages/:packageName/:inventoryId'
-                                element={<NavigateToSystem />} />
-                            <Route path='/templates' element={<Templates />} />
-                            <Route path='/templates/:templateName' element={<TemplateDetail />} />
-                            <Route path='*' element={<Navigate to="advisories" />} />
-                        </Route>
+                <Route path='*' element={
+                    <AsyncComponent
+                        appId="content_management_zero_state"
+                        appName="dashboard"
+                        module="./AppZeroState"
+                        scope="dashboard"
+                        ErrorComponent={<div>Error state</div>}
+                        app="Content_management"
+                        customFetchResults={ hasSystems }
+                    >
+                        <Routes>
+                            <Route element={<PermissionRoute requiredPermissions={generalPermissions} />}>
+                                <Route path='/advisories' element={<Advisories />} />
+                                <Route path='/advisories/:advisoryId/:inventoryId'
+                                    element={<NavigateToSystem />} />
+                                <Route path='/advisories/:advisoryId' element={<AdvisoryPage />} />
+                                <Route path='/systems' element={<Systems />} />
+                                <Route path='/systems/:inventoryId' element={<InventoryDetail />} />
+                                <Route path='/packages' element={<PackagesPage />} />
+                                <Route path='/packages/:packageName' element={<PackageDetail />} />
+                                <Route path='/packages/:packageName/:inventoryId'
+                                    element={<NavigateToSystem />} />
+                                <Route path='/templates' element={<Templates />} />
+                                <Route path='/templates/:templateName' element={<TemplateDetail />} />
+                                <Route path='*' element={<Navigate to="advisories" />} />
+                            </Route>
+                        </Routes>
 
-                    )
-                }
+                    </AsyncComponent>
+                } />
+
             </Routes>
         </Suspense>
     );
