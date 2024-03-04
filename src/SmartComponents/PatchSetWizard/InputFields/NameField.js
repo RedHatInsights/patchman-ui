cf22541d179a240fc59b1afb06dcbae6c3380912
Georgii Karataev <georgkarat@gmail.com>
2024-03-04 14:17:40 +0100

    chore: Fix form helper text for set template wizard

--- src/SmartComponents/PatchSetWizard/InputFields/NameField.js
+++ src/SmartComponents/PatchSetWizard/InputFields/NameField.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import {
     FormGroup,
+    FormHelperText,
+    HelperText,
+    HelperTextItem,
     TextInput
 } from '@patternfly/react-core';
 import useFormApi from '@data-driven-forms/react-form-renderer/use-form-api';
@@ -15,36 +18,46 @@ const NameField = (props) => {
     const values = formOptions.getState()?.values;
 
     const [name, setName] = useState(values?.name);
+    const [validated, setValidated] = useState();
 
-    const { takenBaselineNames, takenBaselineNamesLoading } =
-        useSelector(({ SpecificPatchSetReducer }) => SpecificPatchSetReducer, shallowEqual);
+    const { takenBaselineNames, takenBaselineNamesLoading } = useSelector(
+        ({ SpecificPatchSetReducer }) => SpecificPatchSetReducer,
+        shallowEqual
+    );
 
     useEffect(() => {
+        const validateName = () => {
+            if (
+                values.name === undefined ||
+                values.name === values.previousName
+            ) {
+                return 'default';
+            }
+
+            if (takenBaselineNames.includes(values.name)) {
+                return 'error';
+            }
+
+            return 'success';
+        };
+
         setName(values.name);
+        setValidated(validateName());
     }, [values.name]);
 
     useEffect(() => {
         formOptions.change('takenBaselineNames', takenBaselineNames);
-        formOptions.change('takenBaselineNamesLoading', takenBaselineNamesLoading);
+        formOptions.change(
+            'takenBaselineNamesLoading',
+            takenBaselineNamesLoading
+        );
     }, [takenBaselineNames, takenBaselineNamesLoading]);
-
-    const validateName = () => {
-        if (name === undefined || name === values.previousName) {
-            return;
-        }
-
-        if (takenBaselineNames.includes(name)) {
-            return 'error';
-        }
-    };
 
     return (
         <FormGroup
             fieldId="name"
             label={intl.formatMessage(messages.labelsColumnsName)}
             isRequired
-            helperTextInvalid={intl.formatMessage(messages.templateWizardValidateNameTaken)}
-            validated={validateName()}
         >
             <TextInput
                 type="text"
@@ -56,8 +69,19 @@ const NameField = (props) => {
                 }}
                 aria-label="name"
                 autoFocus
-                validated={validateName()}
+                validated={validated}
             />
+            {validated === 'error' && (
+                <FormHelperText>
+                    <HelperText>
+                        <HelperTextItem variant={validated}>
+                            {intl.formatMessage(
+                                messages.templateWizardValidateNameTaken
+                            )}
+                        </HelperTextItem>
+                    </HelperText>
+                </FormHelperText>
+            )}
         </FormGroup>
     );
 };
