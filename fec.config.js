c884b8bd892d5b461797e412fb1d79b795ef86a3
Muslimjon <mkholjur@redhat.com>
2024-04-05 14:23:22 +0200

    chore(RHINENG-7042): adopt fec-config

---
+++ fec.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+
+module.exports = {
+    appUrl: '/insights/patch',
+    debug: true,
+    useProxy: process.env.PROXY === 'true',
+    proxyVerbose: true,
+    interceptChromeConfig: false,
+    plugins: [],
+    ...(process.env.HOT ? { hotReload: process.env.HOT === 'true' } : { hotReload: true }),
+    ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
+    moduleFederation: {
+        exclude: ['react-router-dom'],
+        shared: [
+            {
+                'react-router-dom': {
+                    singleton: true,
+                    import: false,
+                    version: '^6.8.1',
+                    requiredVersion: '^6.8.1'
+                }
+            }
+        ],
+        exposes: {
+            './RootApp': resolve(__dirname, './src/AppEntry'),
+            './SystemDetail': resolve(__dirname, './src/SmartComponents/SystemDetail/SystemDetail.js')
+        }
+    }
+};
--- src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Patchman from './AppEntry';
-
-ReactDOM.render(
-    <Patchman />,
-    document.getElementById('root')
-);
