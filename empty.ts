47d979f01cefac6127b2c69a66592ac1fee36b42
Andrew Dewar <andrewgdewar@gmail.com>
2024-10-17 15:04:30 -0600

    refactor(ContentSources): RHINENG-7807 Support new content templates

---
+++ empty.ts
@@ -0,0 +1,1 @@
+//Required for tsconfig to build correctly.
--- fec.config.js
+++ fec.config.js
@@ -1,27 +1,56 @@
-const { resolve } = require('path');
+const path = require("path");
+const { dependencies, insights } = require("./package.json");
+
+const moduleName = insights.appname.replace(/-(\w)/g, (_, match) =>
+    match.toUpperCase()
+);
+const srcDir = path.resolve(__dirname, "./src");
 
 module.exports = {
-    appUrl: '/insights/patch',
-    debug: true,
-    useProxy: process.env.PROXY === 'true',
-    proxyVerbose: true,
-    plugins: [],
-    ...(process.env.HOT ? { hotReload: process.env.HOT === 'true' } : { hotReload: true }),
+    appName: moduleName,
+    appUrl: "/insights/patch",
+    useProxy: true,
+    ...(process.env.HOT
+        ? { hotReload: process.env.HOT === "true" }
+        : { hotReload: true }),
     ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
     moduleFederation: {
+        moduleName,
+        exposes: {
+            "./RootApp": path.resolve(__dirname, "./src/AppEntry"),
+            "./SystemDetail": path.resolve(__dirname, "./src/index.js"),
+        },
         shared: [
             {
-                'react-router-dom': {
+                "react-router-dom": {
                     singleton: true,
                     import: false,
-                    version: '^6.8.1',
-                    requiredVersion: '>=6.0.0 <7.0.0'
-                }
-            }
+                    version: dependencies["react-router-dom"],
+                    requiredVersion: ">=6.0.0 <7.0.0",
+                },
+            },
+            {
+                "@unleash/proxy-client-react": {
+                    version: dependencies["@unleash/proxy-client-react"],
+                    singleton: true,
+                },
+            },
         ],
-        exposes: {
-            './RootApp': resolve(__dirname, './src/AppEntry'),
-            './SystemDetail': resolve(__dirname, './src/index.js')
-        }
-    }
+    },
+    /**
+     * Add additional webpack plugins
+     */
+    resolve: {
+        modules: [srcDir, path.resolve(__dirname, "./node_modules")],
+    },
+    routes: {
+        ...(process.env.BACKEND_PORT && {
+            "/api/content-sources/": {
+                // This can be used to run against a local version of the associated backend
+                // Simplift add to the package.json scripts: "local": "BACKEND_PORT=8000 npm start",
+                // Your environment is then only to auth against.
+                host: `http://localhost:${process.env.BACKEND_PORT}`,
+            },
+        }),
+    },
 };
