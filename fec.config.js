79ea2c93e89dacbf05b255f3a0360eafcdef8b79
Andrew Dewar <andrewgdewar@gmail.com>
2024-10-21 09:32:20 -0600

    Feedback from review

--- fec.config.js
+++ fec.config.js
@@ -1,31 +1,25 @@
-const path = require("path");
-const { dependencies, insights } = require("./package.json");
-
-const moduleName = insights.appname.replace(/-(\w)/g, (_, match) =>
-    match.toUpperCase()
-);
-
-const srcDir = path.resolve(__dirname, "./src");
+const path = require('path');
+const { dependencies, insights } = require('./package.json');
 
 module.exports = {
-    appName: moduleName,
-    appUrl: "/insights/patch",
-    useProxy: process.env.PROXY === "true",
+    appName: insights.appname,
+    appUrl: '/insights/patch',
+    useProxy: process.env.PROXY === 'true',
     moduleFederation: {
-        moduleName,
+        moduleName: insights.appname,
         exposes: {
-            "./RootApp": path.resolve(__dirname, "./src/AppEntry"),
-            "./SystemDetail": path.resolve(__dirname, "./src/index.js"),
+            './RootApp': path.resolve(__dirname, './src/AppEntry'),
+            './SystemDetail': path.resolve(__dirname, './src/index.js')
         },
         shared: [
             {
-                "react-router-dom": {
+                'react-router-dom': {
                     singleton: true,
                     import: false,
-                    version: dependencies["react-router-dom"],
-                    requiredVersion: ">=6.0.0 <7.0.0",
-                },
-            },
-        ],
-    },
+                    version: dependencies['react-router-dom'],
+                    requiredVersion: '>=6.0.0 <7.0.0'
+                }
+            }
+        ]
+    }
 };
