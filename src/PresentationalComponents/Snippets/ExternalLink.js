e29eee228981c37eb3f2b0ca08c496d9e7941d4c
Samuel Olekšák <soleksak@redhat.com>
2025-01-06 20:28:30 +0100

    feat(Advisories header): Add tooltip with documentation (#1229)

--- src/PresentationalComponents/Snippets/ExternalLink.js
+++ src/PresentationalComponents/Snippets/ExternalLink.js
@@ -1,28 +1,17 @@
-import { Flex, FlexItem, Icon } from '@patternfly/react-core';
 import ExternalLinkAltIcon from '@patternfly/react-icons/dist/js/icons/external-link-alt-icon';
 import propTypes from 'prop-types';
 import React from 'react';
 
-const ExternalLink = ({ link, text }) => {
-    return (
-        <a
-            href={link}
-            target="__blank"
-        >
-            <Flex flex={{ default: 'inlineFlex' }}>
-                <FlexItem spacer={{ default: 'spacerSm' }}>
-                    {text}
-                </FlexItem>
-                <FlexItem spacer={{ default: 'spacerSm' }}>
-                    <Icon>
-                        <ExternalLinkAltIcon />
-                    </Icon>
-                </FlexItem>
-            </Flex>
-        </a>
-
-    );
-};
+const ExternalLink = ({ link, text }) => (
+    <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+    >
+        {text}
+        <ExternalLinkAltIcon className="pf-v5-u-ml-xs" />
+    </a>
+);
 
 ExternalLink.propTypes = {
     link: propTypes.string,
