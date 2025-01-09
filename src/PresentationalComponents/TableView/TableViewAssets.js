9fa373d7e09f720de3b36f242044249559f699fc
leSamo <soleksak@redhat.com>
2025-01-09 23:19:00 +0100

    Add column mgmt to System Packages table

--- src/PresentationalComponents/TableView/TableViewAssets.js
+++ src/PresentationalComponents/TableView/TableViewAssets.js
@@ -100,32 +100,45 @@ export const systemPackagesColumns = [
     {
         title: intl.formatMessage(messages.labelsColumnsName),
         transforms: [sortable, cellWidth(20)],
-        key: 'name'
+        key: 'name',
+        isShown: true,
+        isShownByDefault: true,
+        isUntoggleable: true
     },
     {
         title: intl.formatMessage(messages.labelsColumnsInstalledVersion),
         transforms: [sortable, cellWidth(15)],
-        key: 'evra'
+        key: 'evra',
+        isShown: true,
+        isShownByDefault: true
     },
     {
         title: intl.formatMessage(messages.labelsColumnsLatestInstallableVersion),
         transforms: [cellWidth(15)],
-        key: 'latest_installable_version'
+        key: 'latest_installable_version',
+        isShown: true,
+        isShownByDefault: true
     },
     {
         title: intl.formatMessage(messages.labelsColumnsLatestApplicableVersion),
         transforms: [cellWidth(15)],
-        key: 'latest_applicable_version'
+        key: 'latest_applicable_version',
+        isShown: true,
+        isShownByDefault: true
     },
     {
         title: intl.formatMessage(messages.labelsColumnsStatus),
         transforms: [sortable, cellWidth(10)],
-        key: 'update_status'
+        key: 'update_status',
+        isShown: true,
+        isShownByDefault: true
     },
     {
         title: intl.formatMessage(messages.labelsColumnsSummary),
         transforms: [sortable, cellWidth(40)],
-        key: 'summary'
+        key: 'summary',
+        isShown: true,
+        isShownByDefault: true
     }
 ];
 
