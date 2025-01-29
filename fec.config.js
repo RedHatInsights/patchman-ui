8d6e0d0d439bd3190c6a15ce690be918573b1854
Adonis Puente <adonis.puente@gmail.com>
2025-01-29 14:16:23 -0600

    feat(RHINENG-15384): Onboard Patchman to Sentry instance

--- fec.config.js
+++ fec.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
 const { dependencies, insights } = require('./package.json');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
     appName: insights.appname,
     appUrl: '/insights/patch',
     useProxy: process.env.PROXY === 'true',
+    devtool: 'hidden-source-map',
+    plugins: [
+        // Put the Sentry Webpack plugin after all other plugins
+        ...(process.env.ENABLE_SENTRY
+            ? [
+                sentryWebpackPlugin({
+                    ...(process.env.SENTRY_AUTH_TOKEN && {
+                        authToken: process.env.SENTRY_AUTH_TOKEN
+                    }),
+                    org: 'red-hat-it',
+                    project: 'patchman-rhel',
+                    moduleMetadata: ({ release }) => ({
+                        dsn: `https://7308344e3a96d7a5c31a2d3899328f10@o490301.ingest.us.sentry.io/4508683262951424`,
+                        org: 'red-hat-it',
+                        project: 'patchman-rhel',
+                        release
+                    })
+                })
+            ]
+            : [])
+    ],
     moduleFederation: {
         moduleName: insights.appname,
         exposes: {
