eebcadbbfec4eb6324d92926014bab3211637a10
TenSt <smaksymc@redhat.com>
2025-12-16 17:39:30 +0100

    feat(tests): add create remediation plan pw test

--- playwright/UI/SystemsTests.spec.ts
+++ playwright/UI/SystemsTests.spec.ts
@@ -1,4 +1,12 @@
-import { test, expect, navigateToSystems, getRowByName, getRowCellByHeader } from 'test-utils';
+import {
+  test,
+  expect,
+  navigateToSystems,
+  getRowByName,
+  getRowCellByHeader,
+  waitForTableLoad,
+  closePopupsIfExist,
+} from 'test-utils';
 
 test.describe('Systems Tests', () => {
   test('Verify the shown system data matches the archive', async ({ page, request, systems }) => {
@@ -16,6 +24,7 @@ test.describe('Systems Tests', () => {
     ];
 
     await navigateToSystems(page);
+    await closePopupsIfExist(page);
     const row = await getRowByName(page, system.name);
 
     await test.step('Check systems list page data', async () => {
@@ -36,6 +45,59 @@ test.describe('Systems Tests', () => {
       await expect(
         page.getByRole('tabpanel', { name: 'Packages' }).locator('#options-menu-top-toggle'),
       ).toContainText(`${attributes.packages_installable}`);
+    });
+  });
+
+  test('Create a new remediation plan', async ({ page, systems }) => {
+    const system = await systems.add('system-remediation-plan-test', 'base');
+
+    await navigateToSystems(page);
+    await closePopupsIfExist(page);
+    const row = await getRowByName(page, system.name);
+
+    await row.getByRole('link', { name: system.name }).click();
+    const planButton = page.getByRole('button', { name: 'Plan remediation' });
+    await expect(planButton).toBeDisabled();
+
+    await test.step('Select an advisory and check that the button is enabled', async () => {
+      const checkbox = page.getByRole('checkbox', { name: 'Select row 0' });
+      await checkbox.check();
+      await expect(checkbox).toBeChecked();
+
+      await expect(planButton).toBeEnabled();
+    });
+
+    await test.step('Click the button and create a plan', async () => {
+      await planButton.click();
+      await expect(page.getByText('Plan a remediation')).toBeVisible();
+
+      await page.getByRole('combobox', { name: 'Type to filter' }).fill(system.name);
+      await page.locator('#select-typeahead-create').click();
+      await page.getByRole('button', { name: 'Create plan' }).click();
+      await expect(page.locator('h1').getByText(system.name)).toBeVisible();
+      await expect(page.getByRole('heading', { name: 'Remediation plan details and' })).toBeVisible(
+        { timeout: 180_000 },
+      );
+    });
+
+    await test.step('Check actions and systems tabs', async () => {
+      await page.getByRole('tab', { name: 'ActionTab' }).click();
+      await waitForTableLoad(page);
+      await expect(page.locator('section > table > tbody > tr')).toHaveCount(1);
+
+      await page.getByRole('tab', { name: 'SystemTab' }).click();
+      await waitForTableLoad(page);
+      await expect(await getRowByName(page, system.name)).toBeVisible();
+      await expect(page.locator('section > table > tbody > tr')).toHaveCount(1);
+    });
+
+    await test.step('Delete remediation plan', async () => {
+      page.locator('div:nth-child(3) > .pf-v6-c-menu-toggle').first().click();
+      page.getByRole('menuitem', { name: 'Delete' }).click();
+      await expect(page.getByText('Delete remediation plan?')).toBeVisible();
+      page.getByRole('button', { name: 'Delete' }).click();
+
+      await expect(page.getByRole('heading', { name: 'Success alert: Deleted' })).toBeVisible();
     });
   });
 });
