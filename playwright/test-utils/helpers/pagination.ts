cedc0ca36801773c2b26c53c05cb886027336837
TenSt <smaksymc@redhat.com>
2026-02-09 16:05:15 +0100

    test(HMS-10052): test pagination on packages page

--- playwright/test-utils/helpers/pagination.ts
+++ playwright/test-utils/helpers/pagination.ts
@@ -8,3 +8,38 @@ export async function getTotalCountFromPagination(page: Page): Promise<number> {
   }
   throw new Error(`Could not extract total count from pagination: ${paginationText}`);
 }
+
+export const commonNonInventoryPaginationLocators = (page: Page) => {
+  const currentPageNumber = page.getByRole('spinbutton', { name: 'Current page' });
+  const pageCountLocator = page.locator('.pf-v6-c-pagination__nav-page-select > span').last();
+  const toolbarTopPaginationToggle = page.locator('#options-menu-top-toggle');
+  const toolbarBottomPaginationToggle = page.locator(
+    '#pagination-options-menu-bottom-bottom-toggle',
+  );
+  const topPreviousButton = page
+    .locator('#options-menu-top-pagination')
+    .getByRole('button', { name: 'Go to previous page' });
+  const topNextButton = page
+    .locator('#options-menu-top-pagination')
+    .getByRole('button', { name: 'Go to next page' });
+  const bottomNextButton = page
+    .locator('#pagination-options-menu-bottom-bottom-pagination')
+    .getByRole('button', { name: 'Go to next page' });
+  const bottomPreviousButton = page
+    .locator('#pagination-options-menu-bottom-bottom-pagination')
+    .getByRole('button', { name: 'Go to previous page' });
+  const bottomLastPageButton = page.getByRole('button', { name: 'Go to last page' });
+  const bottomFirstPageButton = page.getByRole('button', { name: 'Go to first page' });
+  return {
+    currentPageNumber,
+    pageCountLocator,
+    toolbarTopPaginationToggle,
+    toolbarBottomPaginationToggle,
+    topPreviousButton,
+    topNextButton,
+    bottomNextButton,
+    bottomPreviousButton,
+    bottomLastPageButton,
+    bottomFirstPageButton,
+  };
+};
