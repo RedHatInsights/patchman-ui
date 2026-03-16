5061487b48c3ba0e048fb76bf109307338febdb3
TenSt <smaksymc@redhat.com>
2026-03-16 17:58:06 +0100

    fix(tests): fix wording for version-locked system

--- playwright/UI/SystemsTests.spec.ts
+++ playwright/UI/SystemsTests.spec.ts
@@ -177,7 +177,9 @@ test.describe('Systems Tests', () => {
       // using an OUIA attribute selector here, other tests depend on the default data-testid attribute
       // so we can't change the testIdAttribute globally
       await systemRow.locator('[data-ouia-component-id="system-list-warning-icon"]').hover();
-      await expect(page.getByText('Cannot associate system with a template')).toBeVisible();
+      await expect(
+        page.getByText('Remove the version lock to assign a standard template.'),
+      ).toBeVisible();
       await expect(page.getByText(`RHEL is locked at version ${version}`)).toBeVisible();
     });
   });
