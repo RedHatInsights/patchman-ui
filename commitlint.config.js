d0b7ce998053563899f78535b6780930ecda244e
rverdile <ryanverdile@gmail.com>
2026-04-30 13:37:39 -0400

    chore: increase title length to 70 (#1596)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 50], // Enforce 50 character max for commit title
+    'header-max-length': [2, 'always', 70], // Enforce 70 character max for commit title
     'body-max-line-length': [2, 'always', 72], // Wrap body lines at 72 characters
     'subject-case': [0, 'always'],
     'footer-max-length': [0, 'always'],
