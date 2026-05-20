f76c46dca53e8bb69e3443a12c2c077ab9458d0f
Bryttanie House <bhouse@redhat.com>
2026-05-20 10:28:00 -0400

    fix(test): remove workaround for multi user tests
    This reverts the workaround we had that split users into different UI
    projects to prevent auth state conflicts. The conflicts were caused by
    the frontend-development-proxy caching parallel responses by URL, and
    this has been fixed.

--- playwright/UI/AdvisoriesTests.spec.ts
+++ playwright/UI/AdvisoriesTests.spec.ts
@@ -9,6 +9,13 @@ import {
 import { cleanupRemediationPlan } from 'test-utils/helpers/cleaners';
 
 test.describe('Advisories Tests', () => {
+  test.use({
+    storageState: '.auth/ADVISORY_REMEDIATION_TOKEN.json',
+    extraHTTPHeaders: process.env.ADVISORY_REMEDIATION_TOKEN
+      ? { Authorization: process.env.ADVISORY_REMEDIATION_TOKEN }
+      : {},
+  });
+  
   // Disable retries for this test, systems are not deleted immediately in patch so second attempt will likely fail
   test.describe.configure({ retries: 0 });
 
