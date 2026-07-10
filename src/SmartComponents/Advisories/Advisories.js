f850387e358be658003b0505ea91ad48137537a5
Jakub Dugovič <jdugovic@redhat.com>
2026-07-10 17:54:33 +0200

    feat: add OpenSourceBadge to advisories page
    HMS-10988

--- src/SmartComponents/Advisories/Advisories.js
+++ src/SmartComponents/Advisories/Advisories.js
@@ -1,4 +1,5 @@
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
+import { OpenSourceBadge } from '@redhat-cloud-services/frontend-components';
 import React, { useEffect, useState, useMemo, useCallback, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import messages from '../../Messages';
@@ -176,6 +177,9 @@ const Advisories = () => {
                 />
               </Icon>
             </Popover>
+            <span style={{ verticalAlign: '2px' }}>
+              <OpenSourceBadge size='sm' repositoriesURL='https://github.com/RedHatInsights/patchman-ui' />
+            </span>
           </>
         }
         headerOUIA='advisories'
