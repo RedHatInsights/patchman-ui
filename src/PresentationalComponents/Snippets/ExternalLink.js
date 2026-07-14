15df9a2b4a0748c226786b84690a50528698d999
Jakub Dugovič <71605041+Dugowitch@users.noreply.github.com>
2026-07-14 09:52:36 +0200

    fix: prevent external link icon from wrapping (#1682)
    HMS-10988
    
    This commit fixes the issue that when the text is just the right length
    the external link icon would wrap onto a new line and be there alone.
    The added zero-width joiner will ensure no wrapping just before it and
    the span with no-wrap class glues the joiner with the icon, therefore
    the text can wrap but the icon will always have at least one word with
    it on the same line.

--- src/PresentationalComponents/Snippets/ExternalLink.js
+++ src/PresentationalComponents/Snippets/ExternalLink.js
@@ -5,7 +5,10 @@ import React from 'react';
 const ExternalLink = ({ link, text }) => (
   <a href={link} target='_blank' rel='noreferrer'>
     {text}
-    <ExternalLinkAltIcon className='pf-v6-u-ml-xs' />
+    <span className='pf-v6-u-text-nowrap'>
+      {'\u200d'}
+      <ExternalLinkAltIcon className='pf-v6-u-ml-xs' />
+    </span>
   </a>
 );
 
