ff9609353fc8d91c59b26b65b010d89fefc5e4f9
Jakub Dugovič <jdugovic@redhat.com>
2026-08-21 10:51:37 +0200

    fix: fix table column widths
    HMS-11119

--- src/PresentationalComponents/TableView/TableViewAssets.js
+++ src/PresentationalComponents/TableView/TableViewAssets.js
@@ -1,4 +1,4 @@
-import { cellWidth, expandable, sortable } from '@patternfly/react-table';
+import { expandable, sortable, wrappable, nowrap } from '@patternfly/react-table';
 import messages from '../../Messages';
 import { intl } from '../../Utilities/IntlProvider';
 
@@ -6,53 +6,38 @@ export const advisoriesColumns = [
   {
     title: intl.formatMessage(messages.labelsColumnsName),
     cellFormatters: [expandable],
-    transforms: [sortable, cellWidth(15)],
+    transforms: [sortable, wrappable],
     key: 'id',
-    isShown: true,
-    isShownByDefault: true,
-    isUntoggleable: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsSynopsis),
-    transforms: [sortable],
+    transforms: [sortable, nowrap],
     key: 'synopsis',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsType),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'advisory_type_name',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsSeverity),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'severity',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsAffectedSystems),
-    transforms: [sortable, cellWidth(15)],
+    transforms: [sortable, wrappable],
     key: 'applicable_systems',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsReboot),
-    transforms: [sortable],
+    transforms: [sortable, wrappable],
     key: 'reboot_required',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsPublishDate),
-    transforms: [sortable, cellWidth(15)],
+    transforms: [sortable, wrappable],
     key: 'public_date',
-    isShown: true,
-    isShownByDefault: true,
   },
 ];
 
@@ -60,99 +45,71 @@ export const systemAdvisoriesColumns = [
   {
     title: intl.formatMessage(messages.labelsColumnsName),
     cellFormatters: [expandable],
-    transforms: [sortable, cellWidth(15)],
+    transforms: [sortable, wrappable],
     key: 'id',
-    isShown: true,
-    isShownByDefault: true,
-    isUntoggleable: true,
   },
   {
-    title: 'Synopsis',
-    transforms: [sortable, cellWidth(35)],
+    title: intl.formatMessage(messages.labelsColumnsSynopsis),
+    transforms: [sortable, nowrap],
     key: 'synopsis',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsStatus),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'status',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsType),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'advisory_type_name',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsSeverity),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'severity',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsReboot),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'reboot_required',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsPublishDate),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'public_date',
-    isShown: true,
-    isShownByDefault: true,
   },
 ];
 
 export const systemPackagesColumns = [
   {
     title: intl.formatMessage(messages.labelsColumnsName),
-    transforms: [sortable, cellWidth(20)],
+    transforms: [sortable, wrappable],
     key: 'name',
-    isShown: true,
-    isShownByDefault: true,
-    isUntoggleable: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsInstalledVersion),
-    transforms: [sortable, cellWidth(15)],
+    transforms: [sortable, wrappable],
     key: 'evra',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsLatestInstallableVersion),
-    transforms: [cellWidth(15)],
+    transforms: [wrappable],
     key: 'latest_installable_version',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsLatestApplicableVersion),
-    transforms: [cellWidth(15)],
+    transforms: [wrappable],
     key: 'latest_applicable_version',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsStatus),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     key: 'update_status',
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     title: intl.formatMessage(messages.labelsColumnsSummary),
-    transforms: [sortable, cellWidth(40)],
+    transforms: [sortable, wrappable],
     key: 'summary',
-    isShown: true,
-    isShownByDefault: true,
   },
 ];
 
@@ -160,63 +117,52 @@ export const packagesColumns = [
   {
     key: 'name',
     title: intl.formatMessage(messages.labelsColumnsName),
-    transforms: [sortable, cellWidth(25)],
-    isShown: true,
-    isShownByDefault: true,
-    isUntoggleable: true,
+    transforms: [sortable, wrappable],
   },
   {
     key: 'systems_installed',
     title: intl.formatMessage(messages.labelsColumnsInstalledSystems),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     props: {
       width: 10,
     },
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     key: 'systems_applicable',
     title: intl.formatMessage(messages.labelsColumnsApplicableSystems),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     props: {
       width: 10,
     },
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     key: 'systems_installable',
     title: intl.formatMessage(messages.labelsColumnsInstallableSystems),
-    transforms: [sortable, cellWidth(10)],
+    transforms: [sortable, wrappable],
     props: {
       width: 10,
     },
-    isShown: true,
-    isShownByDefault: true,
   },
   {
     key: 'summary',
     title: intl.formatMessage(messages.labelsColumnsSummary),
-    transforms: [sortable, cellWidth(40)],
+    transforms: [sortable, wrappable],
     props: {
       width: 30,
     },
-    isShown: true,
-    isShownByDefault: true,
   },
 ];
 
 export const cvesTableColumns = [
   {
     title: intl.formatMessage(messages.labelsColumnsCveID),
-    transforms: [sortable, cellWidth(40)],
+    transforms: [sortable, nowrap],
     key: 'synopsis',
   },
   {
     key: 'impact',
     title: intl.formatMessage(messages.labelsColumnsSeverity),
-    transforms: [sortable, cellWidth(30)],
+    transforms: [sortable],
     props: {
       width: 30,
     },
@@ -224,7 +170,7 @@ export const cvesTableColumns = [
   {
     key: 'cvss',
     title: intl.formatMessage(messages.labelsColumnsCVSS),
-    transforms: [sortable, cellWidth(30)],
+    transforms: [sortable],
     props: {
       width: 30,
     },
